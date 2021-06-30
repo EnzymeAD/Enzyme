2513a1d63a21a8ea0c0ecfe960f37194f73dfa90
William S. Moses <gh@wsmoses.com>
2021-06-30 18:40:05 -0400

    Fix test case

--- enzyme/test/Integration/ReverseMode/mpi_bcast.c
+++ enzyme/test/Integration/ReverseMode/mpi_bcast.c
@@ -1,4 +1,11 @@
-
+// RUN: %clang -std=c11 -O0 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O1 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O2 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O3 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O0 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
+// RUN: %clang -std=c11 -O1 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
+// RUN: %clang -std=c11 -O2 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
+// RUN: %clang -std=c11 -O3 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
 
 #include <stdio.h>
 #include <math.h>
@@ -6,21 +13,25 @@
 
 #include "test_utils.h"
 
+#include <mpi.h>
+
 double __enzyme_autodiff(void*, ...);
 
 void mpi_bcast_test(double *a, double b, int n) {
   int rank;
-  MPI_COMM_rank(MPI_COMM_WORLD, &rank);
-  double *buf=new double[n];
-  y=0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  double buf[n];
+  double y=0;
+
   if(rank==0) {
-    for(int i=0;1<n;i++) x[i]=x[i]*x[i];
-    MPI_Bcast(x,n,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    for(int i=0;1<n;i++) a[i]=a[i]*a[i];
+    MPI_Bcast(a,n,MPI_DOUBLE,0,MPI_COMM_WORLD);
     MPI_Bcast(buf,n,MPI_DOUBLE,1,MPI_COMM_WORLD);
     for(int i=0;i<n;i++) {
       y+=buf[i];
     }
   }
+
   if(rank==1) {
     MPI_Bcast(buf,n,MPI_DOUBLE,0,MPI_COMM_WORLD);
     for(int i=0;i<n;i++) {
@@ -28,7 +39,6 @@ void mpi_bcast_test(double *a, double b, int n) {
     }
     MPI_Bcast(buf,n,MPI_DOUBLE,1,MPI_COMM_WORLD);
   }
-  delete [] buf;
 }
 
 int main(int argc, char** argv) {
@@ -46,20 +56,19 @@ int main(int argc, char** argv) {
     MPI_Finalize();
     return 0;
   }
-  int N=atoi(argv[1]);
-  double *a=new double[N];
-  double *d_a=new double[N];
+  int N=10;//atoi(argv[1]);
+  double a[N];
+  double d_a[N];
   for(int i=0; i<N; i++)
     d_a[i] = 1.0f; 
 
   double b=0;
   for(int i=0;i<N;i++) a[i]=(double) i;
-  double *a_saved=new double[N];
+  double a_saved[N];
   for(int i=0;i<N;i++) a_saved[i]=a[i];
   
   printf("Ran mpi_bcast");
-  __enzyme_autodiff((void*)mpi_bcast_test, a, d_a, b, n)
+  __enzyme_autodiff((void*)mpi_bcast_test, a, d_a, b, N);
   
 }
-return 0;
 
