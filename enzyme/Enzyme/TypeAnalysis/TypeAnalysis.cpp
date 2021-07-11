abc5ff6d63394018b15b05a19bbd2562218592dd
William S. Moses <gh@wsmoses.com>
2021-07-11 16:09:44 -0400

    Be more inclusive of mpi types [see julia int]

--- enzyme/Enzyme/TypeAnalysis/TypeAnalysis.cpp
+++ enzyme/Enzyme/TypeAnalysis/TypeAnalysis.cpp
@@ -3319,6 +3319,8 @@ void TypeAnalyzer::visitCallInst(CallInst &call) {
     }
 
     /// MPI
+    if (funcName.startswith("PMPI_"))
+        funcName = funcName.substr(1);
     if (funcName == "MPI_Init") {
       TypeTree ptrint;
       ptrint.insert({-1}, BaseType::Pointer);
