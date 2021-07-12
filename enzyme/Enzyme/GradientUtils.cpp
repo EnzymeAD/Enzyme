12b7ab25a2234541d83130c07892e044040365e9
William S. Moses <gh@wsmoses.com>
2021-07-12 04:13:48 -0400

    Additional lulesh mpi fixes

--- enzyme/Enzyme/GradientUtils.cpp
+++ enzyme/Enzyme/GradientUtils.cpp
@@ -3922,6 +3922,7 @@ Value *GradientUtils::lookupM(Value *val, IRBuilder<> &BuilderM,
             }
           }
 
+
           if (ctx && lim && start && offset) {
             Value *firstLim = lim;
             Value *firstStart = start;
@@ -4092,6 +4093,12 @@ Value *GradientUtils::lookupM(Value *val, IRBuilder<> &BuilderM,
   auto found = findInMap(scopeMap, (Value *)inst);
   Value *result = lookupValueFromCache(/*isForwardPass*/ false, BuilderM,
                                        found->second, found->first, isi1);
+  if (result->getType() != inst->getType()) {
+      llvm::errs() << "newFunc: " << *newFunc << "\n";
+      llvm::errs() << "result: " << *result << "\n";
+      llvm::errs() << "inst: " << *inst << "\n";
+      llvm::errs() <<  "val: " << *val << "\n";
+  }
   assert(result->getType() == inst->getType());
   lookup_cache[BuilderM.GetInsertBlock()][val] = result;
   assert(result);
