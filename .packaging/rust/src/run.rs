c6f8b5c83f8c27ee7ef2f05c4e6ef9418056a280
Manuel Drehwald <git@manuel.drehwald.info>
2021-12-08 16:19:50 +0100

    testing a binary as compile-manager

---
+++ .packaging/rust/src/run.rs
@@ -0,0 +1,45 @@
+use std::process::Command;
+
+fn run_and_printerror(command: &mut Command) {
+    println!("Running: `{:?}`", command);
+    match command.status() {
+        Ok(status) => {
+            if !status.success() {
+                panic!("Failed: `{:?}` ({})", command, status);
+            }
+        }
+        Err(error) => {
+            panic!("Failed: `{:?}` ({})", command, error);
+        }
+    }
+}
+
+
+fn main() {
+
+    let build_args = vec![
+    "RUSTFLAGS=--emit=llvm-bc",
+    "cargo", 
+    "+enzyme", 
+    "-Zbuild-std",
+    "rustc",
+    "--target", 
+    "x86_64-unknown-linux-gnu", 
+    "--", 
+    "--emit=llvm-bc",
+    "-g", 
+    "-Copt-level=3"];
+
+    let mut run1 = Command::new("");
+    run1.args(&build_args);
+    run1.arg("-Zno-link");
+    run_and_printerror(&mut run1);
+
+    let mut info_run = Command::new("echo");
+    info_run.arg("First Compilation done, re-compiling now!");
+    run_and_printerror(&mut info_run);
+
+    let mut run2 = Command::new("");
+    run2.args(&build_args);
+    run_and_printerror(&mut run2);
+}
