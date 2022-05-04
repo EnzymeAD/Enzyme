ec46cadfa04b50b6e7a94b5c2b342feb4826eda2
Tim Gymnich <tgymnich@icloud.com>
2022-05-04 09:43:12 +0200

    added indices check

--- enzyme/Enzyme/GradientUtils.cpp
+++ enzyme/Enzyme/GradientUtils.cpp
@@ -445,8 +445,10 @@ Value *GradientUtils::unwrapM(Value *const val, IRBuilder<> &BuilderM,
     SmallVector<std::pair<Value *, unsigned>, 0> insertElements;
     Value *agg = op->getAggregateOperand();
     while (auto op1 = dyn_cast<InsertValueInst>(agg)) {
-      if (knownRecomputeHeuristic.count(op1) == 1 &&
+      if (knownRecomputeHeuristic.count(op1) &&
           knownRecomputeHeuristic[op1] == true)
+        break;
+      if (op1->getNumIndices() != 1)
         break;
       insertElements.push_back({op1->getInsertedValueOperand(),
                                 op1->getInsertedValueOperandIndex()});
