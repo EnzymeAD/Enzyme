801b6ecf85854510a3613dea05a3bed62bffe1a4
Manuel Drehwald <git@manuel.drehwald.info>
2022-05-18 17:32:26 -0400

    Update ActivityAnalysis.cpp

--- enzyme/Enzyme/ActivityAnalysis.cpp
+++ enzyme/Enzyme/ActivityAnalysis.cpp
@@ -85,6 +85,7 @@ const char *KnownInactiveFunctionsStartingWith[] = {
     "$ss5print",
     "_ZNSt7__cxx1112basic_string",
     "_ZNSt7__cxx1118basic_string",
+    "_ZNKSt7__cxx1112basic_string",
     // ostream generic <<
     "_ZStlsISt11char_traitsIcEERSt13basic_ostream",
     "_ZSt16__ostream_insert",
