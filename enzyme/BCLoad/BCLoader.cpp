8ea8a71a5ab97f0f44df6b4efc44d81de24a3e89
Shakil Ahmed <shakil.000024@gmail.com>
2022-06-04 15:13:58 +0000

    New pass manager  code up to date with upstream

--- enzyme/BCLoad/BCLoader.cpp
+++ enzyme/BCLoad/BCLoader.cpp
@@ -6,6 +6,11 @@
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Passes/PassBuilder.h"
+
+#include "BCLoader.h"
+
 #include <map>
 
 #include "llvm/Passes/PassBuilder.h"
@@ -126,7 +131,7 @@ static RegisterPass<BCLoader> X("bcloader",
 ModulePass *createBCLoaderPass() { return new BCLoader(); }
 
 PreservedAnalyses BCLoaderNew::run(Module &M,ModuleAnalysisManager &MAM){
-    return provideDefinitions(M) ? PreservedAnalyses::all() : PreservedAnalyses::none();
+    return provideDefinitions(M) ? PreservedAnalyses::none() : PreservedAnalyses::all();
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
