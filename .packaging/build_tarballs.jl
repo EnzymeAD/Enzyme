282b196c70447d27c6fda40359831edd46cfea90
Valentin Churavy <vchuravy@users.noreply.github.com>
2022-07-05 21:18:13 -0400

    Update .packaging/build_tarballs.jl

--- .packaging/build_tarballs.jl
+++ .packaging/build_tarballs.jl
@@ -44,8 +44,8 @@ ninja -C build-native -j ${nproc} blasheaders enzyme-tblgen
 # 2. Cross-compile
 CMAKE_FLAGS=()
 CMAKE_FLAGS+=(-DENZYME_EXTERNAL_SHARED_LIB=ON)
-CMAKE_FLAGS+=(-DBC_LOAD_HEADER=${WORKSPACE}/build-native/BCLoad/gsl/blas_headers.h)
-CMAKE_FLAGS+=(-DEnzyme_TABLEGEN_EXE=${WORKSPACE}/build-native/bin/enzyme-tblgen)
+CMAKE_FLAGS+=(-DBC_LOAD_HEADER=${WORKSPACE}/Enzyme/build-native/BCLoad/gsl/blas_headers.h)
+CMAKE_FLAGS+=(-DEnzyme_TABLEGEN_EXE=${WORKSPACE}/Enzyme/build-native/bin/enzyme-tblgen)
 CMAKE_FLAGS+=(-DENZYME_CLANG=OFF)
 # RelWithDebInfo for decent performance, with debugability
 CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=RelWithDebInfo)
