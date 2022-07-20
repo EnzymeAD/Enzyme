fdfb22255acf3d67262e00a1cc36637549fbe2d8
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2022-07-20 12:55:31 -0600

    Initial commit for adding custom log1p

---
+++ enzyme/test/Integration/ReverseMode/customlog1p.c
@@ -0,0 +1,34 @@
+// RUN: %clang -std=c11 -O0 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O1 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O2 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O3 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme -S | %lli - 
+// RUN: %clang -std=c11 -O0 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
+// RUN: %clang -std=c11 -O1 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
+// RUN: %clang -std=c11 -O2 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
+// RUN: %clang -std=c11 -O3 %s -S -emit-llvm -o - | %opt - %loadEnzyme -enzyme --enzyme-inline=1 -S | %lli - 
+
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+#include "test_utils.h"
+
+double __enzyme_autodiff(void*, ...);
+
+double log1p_like_function(double a) {
+  return a;
+}
+
+double test(double a) {
+  return log1p_like_function(a);
+}
+
+void* __enzyme_function_like[2] = {(void*)log1p_like_function, "log1p"}; 
+
+int main(int argc, char** argv) {
+
+  double out = __enzyme_autodiff(test, 2.0); 
+  APPROX_EQ(out, 1/3.0, 1e-10); 
+
+  return 0;
+}
