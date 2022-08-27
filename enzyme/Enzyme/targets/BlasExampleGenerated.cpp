10c1ab6442bbc43e546aae02fbaf8e888bccb89b
Manuel Drehwald <git@manuel.drehwald.info>
2022-08-27 02:43:40 +0200

    fix clang-format

--- enzyme/Enzyme/targets/BlasExampleGenerated.cpp
+++ enzyme/Enzyme/targets/BlasExampleGenerated.cpp
@@ -1,3 +1,5 @@
+// clang-format off
+//
 /*===- TableGen'erated file -------------------------------------*- C++ -*-===*\
 |*                                                                            *|
 |* Rewriters                                                                  *|
