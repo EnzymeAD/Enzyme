4f2174227e6ea4e61ced862a048afcb313614432
William S. Moses <gh@wsmoses.com>
2022-09-24 13:17:00 -0400

    Add scf.for

--- enzyme/Enzyme/MLIR/Passes/EnzymeMLIRPass.cpp
+++ enzyme/Enzyme/MLIR/Passes/EnzymeMLIRPass.cpp
@@ -335,7 +335,12 @@ public:
     if (found == originalToNewFnOps.end()) {
       llvm::errs() << oldFunc << "\n";
       llvm::errs() << newFunc << "\n";
-      llvm::errs() << *originst << "\n";
+      for (auto &pair : originalToNewFnOps) {
+        llvm::errs() << " map[" << pair.first << "] = " << pair.second << "\n";
+        // llvm::errs() << " map[" << pair.first << "] = " << pair.second << "
+        // -- " << *pair.first << " " << *pair.second << "\n";
+      }
+      llvm::errs() << originst << " - " << *originst << "\n";
       llvm_unreachable("Could not get new op from original");
     }
     return found->second;
@@ -409,14 +414,32 @@ public:
   bool isConstantValue(mlir::Value v) const {
     if (isa<mlir::IntegerType>(v.getType()))
       return true;
+    if (isa<mlir::IndexType>(v.getType()))
+      return true;
+
+    if (v.getDefiningOp<arith::ConstantOp>())
+      return true;
+
     // TODO
     return false;
   }
-  mlir::Value invertPointerM(mlir::Value v, OpBuilder &BuilderZ) {
+  mlir::Value invertPointerM(mlir::Value v, OpBuilder &Builder2) {
     // TODO
     if (invertedPointers.contains(v))
       return invertedPointers.lookupOrNull(v);
-    return getNewFromOriginal(v);
+
+    if (isConstantValue(v)) {
+      if (auto FT = dyn_cast<mlir::FloatType>(v.getType())) {
+        OpBuilder Builder2(getNewFromOriginal(v.getDefiningOp()));
+        Value dv = Builder2.create<arith::ConstantFloatOp>(
+            v.getLoc(), APFloat(FT.getFloatSemantics(), "0"), FT);
+        invertedPointers.map(v, dv);
+        return dv;
+      }
+      return getNewFromOriginal(v);
+    }
+    llvm::errs() << " could not invert pointer v " << v << "\n";
+    llvm_unreachable("could not invert pointer");
   }
   void setDiffe(mlir::Value val, mlir::Value toset, OpBuilder &BuilderM) {
     /*
@@ -660,6 +683,88 @@ public:
           gutils->setDiffe(mulOp, res, Builder2);
         }
         eraseIfUnused(*op);
+        return;
+      }
+      if (auto addOp = dyn_cast<arith::AddFOp>(op)) {
+        // Derivative of r = a + b -> dr = da + db
+        if (!gutils->isConstantValue(addOp)) {
+          mlir::Value res = nullptr;
+          OpBuilder Builder2(gutils->getNewFromOriginal(op));
+          for (int i = 0; i < 2; i++) {
+            if (!gutils->isConstantValue(addOp.getOperand(i))) {
+              Value tmp = gutils->invertPointerM(addOp.getOperand(i), Builder2);
+              if (res == nullptr)
+                res = tmp;
+              else
+                res = Builder2.create<arith::AddFOp>(addOp.getLoc(), res, tmp);
+            }
+          }
+          gutils->setDiffe(addOp, res, Builder2);
+        }
+        eraseIfUnused(*op);
+        return;
+      }
+      if (auto forOp = dyn_cast<scf::ForOp>(op)) {
+        auto nFor = cast<scf::ForOp>(gutils->getNewFromOriginal(op));
+        OpBuilder Builder2(nFor);
+        SmallVector<Type> nTypes;
+        for (auto r : forOp->getResults()) {
+          // TODO only if used
+          nTypes.push_back(r.getType());
+          if (!gutils->isConstantValue(r))
+            nTypes.push_back(gutils->getShadowType(r.getType()));
+        }
+        SmallVector<Value> nArgs;
+        for (auto r :
+             llvm::zip(forOp.getIterOperands(), forOp.getRegionIterArgs())) {
+          // TODO only if used
+          nArgs.push_back(gutils->getNewFromOriginal(std::get<0>(r)));
+          if (!gutils->isConstantValue(std::get<1>(r)))
+            nArgs.push_back(gutils->invertPointerM(std::get<0>(r), Builder2));
+        }
+        auto repFor = Builder2.create<scf::ForOp>(
+            forOp.getLoc(), gutils->getNewFromOriginal(forOp.getLowerBound()),
+            gutils->getNewFromOriginal(forOp.getUpperBound()),
+            gutils->getNewFromOriginal(forOp.getStep()), nArgs);
+        repFor.getRegion().takeBody(nFor.getRegion());
+
+        SmallVector<Value> reps;
+        size_t idx = 0;
+        for (auto r : forOp.getResults()) {
+          // TODO only if used
+          reps.push_back(repFor.getResult(idx));
+          idx++;
+          if (!gutils->isConstantValue(r)) {
+            auto inverted = gutils->invertedPointers.lookupOrNull(r);
+            assert(inverted);
+            gutils->invertedPointers.map(r, repFor.getResult(idx));
+            inverted.replaceAllUsesWith(repFor.getResult(idx));
+            gutils->erase(inverted.getDefiningOp());
+            idx++;
+          }
+        }
+        nFor.replaceAllUsesWith(reps);
+        gutils->erase(nFor);
+        for (auto &o : llvm::make_early_inc_range(
+                 forOp.getBody()->without_terminator())) {
+          visit(&o);
+        }
+        auto oldYield = repFor.getBody()->getTerminator();
+        Builder2.setInsertionPointToEnd(repFor.getBody());
+        SmallVector<Value> nYields;
+        for (auto r :
+             llvm::zip(forOp.getResults(),
+                       forOp.getBody()->getTerminator()->getOperands())) {
+          // TODO only if used
+          nYields.push_back(gutils->getNewFromOriginal(std::get<1>(r)));
+          if (!gutils->isConstantValue(std::get<0>(r)))
+            nYields.push_back(gutils->invertPointerM(std::get<1>(r), Builder2));
+        }
+        repFor.getBody()->push_back(oldYield->create(
+            oldYield->getLoc(), oldYield->getName(), TypeRange(), nYields,
+            oldYield->getAttrs(), oldYield->getSuccessors(),
+            oldYield->getNumRegions()));
+        gutils->erase(oldYield);
         return;
       }
     }
