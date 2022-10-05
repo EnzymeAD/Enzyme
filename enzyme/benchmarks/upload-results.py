d7a07db53abce47cc16f5235ad11fc152cfb22a5
Tim Gymnich <tgymnich@icloud.com>
2022-10-06 01:19:47 +0200

    Add platform and modify json

--- enzyme/benchmarks/upload-results.py
+++ enzyme/benchmarks/upload-results.py
@@ -3,6 +3,7 @@ import datetime
 import subprocess
 import requests
 import argparse
+import platform
 
 def get_git_revision_short_hash():
     try:
@@ -10,20 +11,27 @@ def get_git_revision_short_hash():
     except:
         return None
 
-def extract_results(json):
+def extract_results(json, llvm, mode):
     result = []
     githash = get_git_revision_short_hash()
     time = datetime.datetime.now().replace(microsecond=0).isoformat()
-    metadata = {}
-    if githash is not None:
-        metadata["githash"] = githash
+    cpu = platform.processor()
     
     for test_suite in json:
         series = test_suite["name"]
         for tool in test_suite["tools"]:
             if "enzyme" in tool["name"].lower():
                 value = tool["runtime"]
-                res = {"series": series, "value": value, "metadata": metadata, "timestamp": time}
+                res = {
+                    "mode": mode,
+                    "llvm_version": llvm,
+                    "testsuite": series,
+                    "commit": githash,
+                    "name": tool,
+                    "elapsed": value,
+                    "timestamp": time,
+                    "platform": cpu
+                }
                 result.append(res)
     return result
 
@@ -32,10 +40,14 @@ parser.add_argument('file', type=argparse.FileType('r'), help="JSON file contain
 parser.add_argument('-t', '--token', type=str, required=False, help="Token to authorize at graphing backend")
 parser.add_argument('-u', '--url', type=str, required=True, help="URL of the graphing backend")
 
+parser.add_argument('--llvm', type=str, required=True, help="LLVM Version")
+parser.add_argument('--mode', type=str, required=True, help="Derivative Mode")
+
+
 args = parser.parse_args()
 
 json = json.load(args.file)
-results = extract_results(json)
+results = extract_results(json, args.llvm, args.mode)
 
 if args.token:
     requests.post(args.url, json=results, headers={"TOKEN": args.token})
