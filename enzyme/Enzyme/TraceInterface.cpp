3eb6e7a742e8e19f0c7c234cb0ea2f029814ba4d
Tim Gymnich <tgymnich@icloud.com>
2023-02-25 18:40:28 -0500

    add license headers

--- enzyme/Enzyme/TraceInterface.cpp
+++ enzyme/Enzyme/TraceInterface.cpp
@@ -1,3 +1,29 @@
+//===- TraceInterface.h - Interact with probabilistic programming traces
+//---===//
+//
+//                             Enzyme Project
+//
+// Part of the Enzyme Project, under the Apache License v2.0 with LLVM
+// Exceptions. See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// If using this code in an academic setting, please cite the following:
+// @incollection{enzymeNeurips,
+// title = {Instead of Rewriting Foreign Code for Machine Learning,
+//          Automatically Synthesize Fast Gradients},
+// author = {Moses, William S. and Churavy, Valentin},
+// booktitle = {Advances in Neural Information Processing Systems 33},
+// year = {2020},
+// note = {To appear in},
+// }
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains an abstraction for static and dynamic implementations of
+// the probabilistic programming interface.
+//
+//===----------------------------------------------------------------------===//
+
 #include "TraceInterface.h"
 
 #include "llvm/IR/Function.h"
