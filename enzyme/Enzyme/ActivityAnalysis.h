ebbdc95b6837eb29b68de8517b049b53f8ad20b7
William Moses <gh@wsmoses.com>
2023-03-09 19:17:33 -0500

    Ensure activity analysis handles recreated structs (#1020)

--- enzyme/Enzyme/ActivityAnalysis.h
+++ enzyme/Enzyme/ActivityAnalysis.h
@@ -116,7 +116,9 @@ public:
       : PPC(PPC), AA(AA_), notForAnalysis(notForAnalysis_), TLI(TLI_),
         ActiveReturns(ActiveReturns), directions(UP | DOWN),
         ConstantValues(ConstantValues.begin(), ConstantValues.end()),
-        ActiveValues(ActiveValues.begin(), ActiveValues.end()) {}
+        ActiveValues(ActiveValues.begin(), ActiveValues.end()) {
+    InsertConstValueRecursionHandler = nullptr;
+  }
 
   /// Return whether this instruction is known not to propagate adjoints
   /// Note that instructions could return an active pointer, but
@@ -137,6 +139,7 @@ private:
       ReEvaluateInstIfInactiveValue;
 
   void InsertConstantInstruction(TypeResults const &TR, llvm::Instruction *I);
+  llvm::SmallVector<llvm::Value *, 1> *InsertConstValueRecursionHandler;
   void InsertConstantValue(TypeResults const &TR, llvm::Value *V);
 
   /// Create a new analyzer starting from an existing Analyzer
@@ -152,6 +155,7 @@ private:
     assert(directions != 0);
     assert((directions & Other.directions) == directions);
     assert((directions & Other.directions) != 0);
+    InsertConstValueRecursionHandler = nullptr;
   }
 
   /// Import known constants from an existing analyzer
@@ -250,4 +254,18 @@ private:
   std::map<std::pair<bool, llvm::Value *>, bool> StoredOrReturnedCache;
 };
 
+constexpr inline const char *to_string(ActivityAnalyzer::UseActivity UA) {
+  switch (UA) {
+  case ActivityAnalyzer::UseActivity::None:
+    return "None";
+  case ActivityAnalyzer::UseActivity::OnlyLoads:
+    return "OnlyLoads";
+  case ActivityAnalyzer::UseActivity::OnlyStores:
+    return "OnlyStores";
+  case ActivityAnalyzer::UseActivity::OnlyNonPointerStores:
+    return "OnlyNonPointerStores";
+  case ActivityAnalyzer::UseActivity::AllStores:
+    return "AllStores";
+  }
+}
 #endif
