4ce8045fd93b6585db2f61a0d2eedce776e220b4
Manuel Drehwald <git@manuel.drehwald.info>
2023-03-20 18:07:12 -0400

    Update enzyme/Enzyme/TypeAnalysis/TypeAnalysis.cpp
    Co-authored-by: Tim Gymnich <tgymnich@icloud.com>

--- enzyme/Enzyme/TypeAnalysis/TypeAnalysis.cpp
+++ enzyme/Enzyme/TypeAnalysis/TypeAnalysis.cpp
@@ -63,7 +63,7 @@ llvm::cl::opt<int> MaxIntOffset("enzyme-max-int-offset", cl::init(100),
                                 cl::Hidden,
                                 cl::desc("Maximum type tree offset"));
 
-llvm::cl::opt<uint> EnzymeMaxTypeDepth("enzyme-max-type-depth", cl::init(6),
+llvm::cl::opt<unsigned> EnzymeMaxTypeDepth("enzyme-max-type-depth", cl::init(6),
                                        cl::Hidden,
                                        cl::desc("Maximum type tree depth"));
 
