d93134e711ebcaf0fc81ed775cdb39f9c7e3c7a3
William Moses <gh@wsmoses.com>
2023-04-10 16:22:36 -0400

    add vector fmax (#1103)
    * add vector fmax
    
    * fix

--- enzyme/Enzyme/AdjointGenerator.h
+++ enzyme/Enzyme/AdjointGenerator.h
@@ -4142,6 +4142,27 @@ public:
         }
         return;
       }
+#if LLVM_VERSION_MAJOR >= 12
+      case Intrinsic::vector_reduce_fmax: {
+        if (vdiff && !gutils->isConstantValue(orig_ops[0])) {
+          auto prev = lookup(gutils->getNewFromOriginal(orig_ops[0]), Builder2);
+          auto lhs = Builder2.CreateExtractElement(prev, (uint64_t)0);
+          auto rhs = Builder2.CreateExtractElement(prev, 1);
+          Value *cmp = Builder2.CreateFCmpOLT(lhs, rhs);
+
+          auto rule = [&](Value *vdiff) {
+            auto nv = Constant::getNullValue(orig_ops[0]->getType());
+            auto lhs_v = Builder2.CreateInsertElement(nv, vdiff, (uint64_t)0);
+            auto rhs_v = Builder2.CreateInsertElement(nv, vdiff, 1);
+            return Builder2.CreateSelect(cmp, rhs_v, lhs_v);
+          };
+          Value *dif0 =
+              applyChainRule(orig_ops[0]->getType(), Builder2, rule, vdiff);
+          addToDiffe(orig_ops[0], dif0, Builder2, I.getType());
+        }
+        return;
+      }
+#endif
 
 #if LLVM_VERSION_MAJOR < 10
       case Intrinsic::x86_sse_min_ss:
@@ -4641,6 +4662,29 @@ public:
         setDiffe(&I, dif, Builder2);
         return;
       }
+#if LLVM_VERSION_MAJOR >= 12
+      case Intrinsic::vector_reduce_fmax: {
+        if (gutils->isConstantInstruction(&I))
+          return;
+        auto prev = gutils->getNewFromOriginal(orig_ops[0]);
+        auto lhs = Builder2.CreateExtractElement(prev, (uint64_t)0);
+        auto rhs = Builder2.CreateExtractElement(prev, 1);
+        Value *cmp = Builder2.CreateFCmpOLT(lhs, rhs);
+
+        auto rule = [&](Value *vdiff) {
+          auto lhs_v = Builder2.CreateExtractElement(vdiff, (uint64_t)0);
+          auto rhs_v = Builder2.CreateExtractElement(vdiff, 1);
+
+          Value *dif = Builder2.CreateSelect(cmp, rhs_v, lhs_v);
+          return dif;
+        };
+        auto vdiff = diffe(orig_ops[0], Builder2);
+
+        Value *dif = applyChainRule(I.getType(), Builder2, rule, vdiff);
+        setDiffe(&I, dif, Builder2);
+        return;
+      }
+#endif
 
 #if LLVM_VERSION_MAJOR < 10
       case Intrinsic::x86_sse_min_ss:
