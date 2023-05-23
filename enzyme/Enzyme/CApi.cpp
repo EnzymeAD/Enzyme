e987a2ee9fac623da23fe38d18a482193e8c52e3
William S. Moses <gh@wsmoses.com>
2023-05-23 18:54:44 -0400

    Fix sret_v

--- enzyme/Enzyme/CApi.cpp
+++ enzyme/Enzyme/CApi.cpp
@@ -1460,6 +1460,7 @@ void EnzymeFixupJuliaCallingConvention(LLVMValueRef F_C) {
       auto I = cast<Instruction>(U.getUser());
       if (I->getParent()->getParent() == F)
         continue;
+      uses.push_back(I);
       op.push_back(U.getOperandNo());
     }
     IRBuilder<> EB(&NewF->getEntryBlock().front());
