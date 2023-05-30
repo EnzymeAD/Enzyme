eca384ccf1fe1869c9119642cfa1959dd7647abc
Manuel Drehwald <git@manuel.drehwald.info>
2023-05-29 22:54:18 -0400

    fix diffUse for cblas case

--- enzyme/tools/enzyme-tblgen/blasDiffUseUpdater.h
+++ enzyme/tools/enzyme-tblgen/blasDiffUseUpdater.h
@@ -1,21 +1,36 @@
 void emit_BLASDiffUse(TGPattern &pattern, raw_ostream &os) {
+  auto argTypeMap = pattern.getArgTypeMap();
+  auto argUsers = pattern.getArgUsers();
+  bool lv23 = pattern.isBLASLevel2or3();
+
   auto name = pattern.getName();
 
   os << "if (blas.function == \"" << name << "\") {\n";
+  os << "const bool byRef = blas.prefix == \"\";\n";
+  if (lv23) {
+    os << "const int offset = (byRef ? 0 : 1);\n";
 
-  auto argTypeMap = pattern.getArgTypeMap();
-  auto argUsers = pattern.getArgUsers();
+    auto layout_users = argUsers.lookup(0);
+    os << "if (!byRef && val == CI->getArgOperand(0)) {\n";
+    for (auto user : layout_users) {
+      os << "  if (!gutils->isConstantValue(CI->getOperand(" << user
+         << "))) return true;\n";
+    }
+    os << "}\n";
+  }
 
-  for (size_t i = 0; i < argTypeMap.size(); i++) {
-    os << "  if (val == CI->getArgOperand(" << i << ")) {\n";
-    auto users = argUsers.lookup(i);
+  for (size_t argPos = (lv23 ? 1 : 0); argPos < argTypeMap.size(); argPos++) {
+    auto users = argUsers.lookup(argPos);
+    size_t i = (lv23 ? argPos - 1 : argPos);
+    os << "  if (val == CI->getArgOperand(" << i << (lv23 ? " + offset" : "")
+       << ")) {\n";
     for (auto a : users) {
       // The following shows that I probably should change the tblgen
       // logic and the Blas.td declaration
-      if (a == i)
+      if (a == i) // a == i? argpos ?
         continue;
       os << "    if (!gutils->isConstantValue(CI->getOperand(" << a
-         << "))) return true;\n";
+         << (lv23 ? " + offset" : "") << "))) return true;\n";
     }
     os << "  }\n";
   }
