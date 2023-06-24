5780a4b33a61674581d84212765fbb879f8eaa75
Dan <tthsqe12@gmail.com>
2023-06-24 15:54:48 -0400

    8 byte tape on all -O levels

--- enzyme/test/Integration/ReverseMode/allocatedtape_err.c
+++ enzyme/test/Integration/ReverseMode/allocatedtape_err.c
@@ -7,15 +7,16 @@
 // RUN: if [ %llvmver -ge 12 ]; then %clang -std=c11 -g -O2 %s -S -emit-llvm -o -  %newLoadClangEnzyme -Xclang -verify; fi
 // RUN: if [ %llvmver -ge 12 ]; then %clang -std=c11 -g -O3 %s -S -emit-llvm -o -  %newLoadClangEnzyme -Xclang -verify; fi
 
+#include <math.h>
+
 extern int enzyme_allocated, enzyme_tape;
 void __enzyme_reverse(void*, ...);
 
 void f(double* x) {
-    x[0] += x[1]*x[1];
-    x[1] = -x[1];
+    x[0] = sin(x[0]);
 }
 
 void f_rev(double* x, double* x_b, void* tape) {
-    __enzyme_reverse(f, x, x_b, enzyme_allocated, 1, enzyme_tape, tape); // expected-error {{Enzyme: need 24 bytes have 1 bytes}}
+    __enzyme_reverse(f, x, x_b, enzyme_allocated, 1, enzyme_tape, tape); // expected-error {{Enzyme: need 8 bytes have 1 bytes}}
 }
 
