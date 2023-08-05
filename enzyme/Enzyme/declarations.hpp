19ed54ddbca853f62dd3c0e37b46052c2ba337d6
Sam Mish <mish2@llnl.gov>
2023-08-04 21:29:57 -0700

    formatting

--- enzyme/Enzyme/declarations.hpp
+++ enzyme/Enzyme/declarations.hpp
@@ -5,8 +5,8 @@ extern int enzyme_dupnoneed;
 extern int enzyme_out;
 extern int enzyme_const;
 
-template < typename return_type, typename ... T >
-return_type __enzyme_fwddiff(void*, T ... );
+template <typename return_type, typename... T>
+return_type __enzyme_fwddiff(void *, T...);
 
-template < typename return_type, typename ... T >
-return_type __enzyme_autodiff(void*, T ... );
+template <typename return_type, typename... T>
+return_type __enzyme_autodiff(void *, T...);
