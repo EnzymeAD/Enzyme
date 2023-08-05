515e6fdeb1fa51f00ebc229b18bfab8b9b557964
Sam Mish <mish2@llnl.gov>
2023-08-04 21:26:24 -0700

    add new header for enzyme declarations

---
+++ enzyme/Enzyme/declarations.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+extern int enzyme_dup;
+extern int enzyme_dupnoneed;
+extern int enzyme_out;
+extern int enzyme_const;
+
+template < typename return_type, typename ... T >
+return_type __enzyme_fwddiff(void*, T ... );
+
+template < typename return_type, typename ... T >
+return_type __enzyme_autodiff(void*, T ... );
