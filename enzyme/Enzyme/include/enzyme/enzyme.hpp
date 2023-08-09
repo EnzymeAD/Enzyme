d03cc58eca7f5a2ee47746e175dd6c258ed9e6e2
Sam Mish <mish2@llnl.gov>
2023-08-09 11:57:43 -0700

    +basic tuple impl and c++ interface prototype

---
+++ enzyme/Enzyme/include/enzyme/enzyme.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+namespace enzyme {
+
+    struct nodiff{};
+
+    template < typename T >
+    struct active{
+      T value;
+      operator T&() { return value; }
+    };
+
+    template < typename T >
+    active(T) -> active<T>;
+
+    template < typename T >
+    struct duplicated{  
+      T value;
+      T shadow;
+    };
+
+    template < typename T >
+    struct inactive{};
+
+    template < typename T >
+    struct type_info {
+      static constexpr bool is_active = false; 
+
+      #ifdef ENZYME_OMIT_INACTIVE
+      using type = tuple<>;
+      #else 
+      using type = tuple<nodiff>;
+      #endif
+    };
+
+    template < typename T >
+    struct type_info < active<T> >{
+        static constexpr bool is_active = true; 
+        using type = tuple<T>;
+    };
+
+    template < typename ... T >
+    struct concatenated;
+
+    template < typename ... S, typename ... T, typename ... rest >
+    struct concatenated < tuple < S ... >, tuple < T ... >, rest ... > {
+        using type = typename concatenated< tuple< S ..., T ... >, rest ... >::type;
+    };
+
+    template < typename ... T >
+    struct concatenated < tuple < T ... > > {
+        using type = tuple< T ... >;
+    };
+
+    template < typename ... T >
+    struct autodiff_return {
+        using type = typename concatenated< 
+            typename type_info<T>::type ...
+        >::type;
+    };
+
+    template < typename function, typename ... arg_types >
+    typename autodiff_return<arg_types...>::type autodiff(function && f, arg_types && ... args) {
+      // TODO
+    }
+
+}
---
+++ enzyme/Enzyme/include/enzyme/tuple.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+///////////////
+// tuple.hpp //
+///////////////
+
+// why reinvent the wheel and implement a tuple class?
+//  - ensure data is laid out in the same order the types are specified
+//        see: https://github.com/EnzymeAD/Enzyme/issues/1191#issuecomment-1556239213
+//  - CUDA compatibility: std::tuple has some compatibility issues when used
+//        in a __device__ context (this may get better in c++20 with the improved
+//        constexpr support for std::tuple). Owning the implementation lets
+//        us add __host__ __device__ annotations to any part of it
+
+#include <utility> // for std::integer_sequence
+
+namespace enzyme {
+
+template <int i>
+struct Index {};
+
+template <int i, typename T>
+struct value_at_position { 
+    T& operator[](Index<i>) { return value; }
+    T value;
+};
+
+template <typename S, typename... T>
+struct tuple_base;
+
+template <int... i, typename... T>
+struct tuple_base<std::integer_sequence<int, i...>, T...>
+    : public value_at_position<i, T>... {
+    using value_at_position<i, T>::operator[]...;
+}; 
+
+template <typename... T>
+struct tuple : public tuple_base<std::make_integer_sequence<int, sizeof...(T)>, T...> {};
+
+template < int i, typename ... T>
+auto & get(tuple< T ... > & tup) {
+    return tup[Index<i>{}];
+}
+
+template < int i, typename ... T>
+const auto & get(const tuple< T ... > & tup) {
+    return tup[Index<i>{}];
+}
+
+}
