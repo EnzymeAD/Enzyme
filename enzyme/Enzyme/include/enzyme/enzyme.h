33f5fabc1fe54d69afc20252b542da6d5b11071d
Sam Mish <mish2@llnl.gov>
2023-09-15 13:09:38 -0700

    use custom apply implementation

--- enzyme/Enzyme/include/enzyme/enzyme.h
+++ enzyme/Enzyme/include/enzyme/enzyme.h
@@ -2,7 +2,8 @@
 
 #include "tuple.h"
 
-#include <tuple> // for std::apply
+#include <tuple>
+#include <utility>
 #include <type_traits>
 
 int enzyme_dup;
@@ -18,7 +19,7 @@ return_type __enzyme_fwddiff(void*, T ... );
 // std::apply(__enzyme_autodiff<...>(...))
 // when this func template is left undefined
 template < typename return_type, typename ... T >
-return_type __enzyme_autodiff(T ... );
+return_type __enzyme_autodiff(void*, T ... );
 #else
 // getting wrong answers when providing a dummy impl
 // in an attempt to work around undef ref issues
@@ -107,29 +108,46 @@ namespace enzyme {
     };
 
     template < typename T >
-    auto splat(const enzyme::duplicated<T> & arg) {
+    auto expand_args(const enzyme::duplicated<T> & arg) {
         return std::tuple<int, T, T>{enzyme_dup, arg.value, arg.shadow};
     }
 
     template < typename T >
-    auto splat(const enzyme::active<T> & arg) {
+    auto expand_args(const enzyme::active<T> & arg) {
         return std::tuple<int, T>{enzyme_out, arg.value};
     }
 
     template < typename T >
-    auto splat(const enzyme::inactive<T> & arg) {
+    auto expand_args(const enzyme::inactive<T> & arg) {
         return std::tuple<int, T>{enzyme_const, arg.value};
     }
 
+    namespace detail {
+        template <class F, class Tuple, std::size_t... I>
+        __attribute__((always_inline))
+        constexpr decltype(auto) apply_impl(F&& f, Tuple&& t, std::index_sequence<I...>) {
+            return f(std::get<I>(std::forward<Tuple>(t))...);
+        }
+
+        template <class F, class Tuple>
+        __attribute__((always_inline))
+        constexpr decltype(auto) apply(F&& f, Tuple&& t)
+        {
+            auto iseq = std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<Tuple>>>{};
+            return apply_impl(std::forward<F>(f), std::forward<Tuple>(t), iseq);
+        }
+
+    }  // namespace detail 
+
     template < typename return_type, typename function, typename ... enz_arg_types >
-    auto autodiff_impl(function && f, std::tuple< enz_arg_types ... > && arg_tup) {
-      return std::apply(__enzyme_autodiff<return_type, enz_arg_types ... >, arg_tup);
+    auto autodiff_impl(function && f, std::tuple< void*, enz_arg_types ... > && arg_tup) {
+      return detail::apply(__enzyme_autodiff<return_type, enz_arg_types ... >, arg_tup);
     }
 
     template < typename function, typename ... arg_types>
     auto autodiff(function && f, arg_types && ... args) {
         using return_type = typename autodiff_return<arg_types...>::type;
-        return autodiff_impl<return_type>(f, std::tuple_cat(std::tuple{f}, splat(args)...));
+        return autodiff_impl<return_type>(f, std::tuple_cat(std::tuple{(void*)f}, expand_args(args)...));
     }
 
 }
