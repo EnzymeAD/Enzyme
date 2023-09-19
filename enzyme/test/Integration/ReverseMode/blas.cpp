c5ebbd763ae4cce05ca6f07d4fc0c3d346319b5d
William S. Moses <gh@wsmoses.com>
2023-09-19 14:09:47 -0400

    Improve blas tests

--- enzyme/test/Integration/ReverseMode/blas.cpp
+++ enzyme/test/Integration/ReverseMode/blas.cpp
@@ -15,17 +15,74 @@
 
 #include "test_utils.h"
 
-#include <map>
 #include <cassert>
-#include <vector>
 #include <string>
-#include <iostream>
-#include <sstream>
+#include <stdlib.h>
+#include <string.h>
+
+template <typename T> 
+class vector {
+    T* data;
+    size_t capacity;
+	size_t length; 
+public:
+    vector() : data(nullptr), capacity(0), length(0) {}
+    vector(const vector &prev) : data((T*)malloc(sizeof(T)*prev.capacity)), capacity(prev.capacity), length(prev.length) {
+		memcpy(data, prev.data, prev.length*sizeof(T));
+	}
+    void operator=(const vector &prev) {
+ 		free(data);
+		data = (T*)malloc(sizeof(T)*prev.capacity);
+		capacity = prev.capacity;
+		length = prev.length;
+		memcpy(data, prev.data, prev.length*sizeof(T));
+    }
+	// Don't destruct to avoi dso handle in global
+    // ~vector() { free(data); }
+ 
+    void push_back(T v) {
+        if (length == capacity) {
+			size_t next = capacity == 0 ? 1 : (2 * capacity);
+			data = (T*)realloc(data, sizeof(T)*next);
+			capacity = next;
+        } 
+        data[length] = v;
+        length++;
+    }
+ 
+    T& operator[](size_t index) {
+ 		assert(index < length);
+		return data[index];
+    }
+    
+	const T& operator[] (size_t index) const {
+ 		assert(index < length);
+		return data[index];
+    }
+
+	bool operator==(const vector& rhs) const {
+		if (length != rhs.length) return false;
+		for (size_t i=0; i<length; i++)
+			if (data[i] != rhs.data[i]) return false;
+		return true;
+	}
+	bool operator!=(const vector& rhs) const {
+		return !(operator==(rhs));
+	}
+    size_t size() const { return length; }
+
+	void clear() {
+		length = 0;
+	} 
+};
+
 
 bool inDerivative = false;
 
     char DEFAULT_LAYOUT = 0x72;
     
+    double *UNUSED_POINTER = (double*)0x00000070;
+
     double *A = (double*)0x00000100;
     double *dA = (double*)0x00000700;
     int incA = 1234;
@@ -46,12 +103,14 @@ bool inDerivative = false;
     int lda = 3416;
 char UNUSED_TRANS = 'A';
 int UNUSED_INT = -1;
+double UNUSED_DOUBLE;
 
 enum class CallType {
     GEMV,
     GEMM,
     SCAL,
-    GER
+    GER,
+    COPY
 };
 
 struct BlasCall {
@@ -71,70 +130,236 @@ struct BlasCall {
     int iarg4;
     int iarg5;
     int iarg6;
+	bool operator==(const BlasCall &rhs) const {
+#define CHECK(A) if (A != rhs.A) return false;
+		CHECK(inDerivative)
+		CHECK(type)
+		CHECK(pout_arg1)
+		CHECK(pin_arg1)
+		CHECK(pout_arg1)
+		CHECK(farg1)
+		CHECK(farg2)
+		CHECK(layout)
+		CHECK(targ1)
+		CHECK(targ2)
+		CHECK(iarg1)
+		CHECK(iarg2)
+		CHECK(iarg3)
+		CHECK(iarg4)
+		CHECK(iarg5)
+		CHECK(iarg6)
+		return true;
+	}
+	bool operator!=(const BlasCall& rhs) const {
+		return !(operator==(rhs));
+	}
 };
 
 
-std::string astr(bool v) {
-  if (v) return "Reverse (true)";
-  else   return "Primal (false)";
+void printty(bool v) {
+  if (v) printf("Reverse (true)");
+  else   printf("Primal (false)");
 }
 
-std::string astr(CallType v) {
+void printty(CallType v) {
   switch (v) {
-      case CallType::GEMV: return "GEMV";
-      case CallType::GEMM: return "GEMM";
-      case CallType::SCAL: return "SCAL";
-      case CallType::GER: return "GER";
-      default: return "UNKNOWN_CALL";
+      case CallType::GEMV: printf("GEMV"); return;
+      case CallType::GEMM: printf("GEMM"); return;
+      case CallType::SCAL: printf("SCAL"); return;
+      case CallType::GER: printf("GER"); return;
+      case CallType::COPY: printf("COPY"); return;
+      default: printf("UNKNOWN CALL (%d)", (int)v);
     }
 }
 
-namespace std {
-std::string to_string(void* v) {
-        std::ostringstream address;
-        address << (void const *)v;
-        return address.str();
+void printty(void* v) {
+    if (v == A) printf("A");
+    else if (v == dA) printf("dA");
+    else if (v == B) printf("B");
+    else if (v == dB) printf("dB");
+    else if (v == C) printf("C");
+    else if (v == dC) printf("dC");
+    else if (v == UNUSED_POINTER) printf("UNUSED_POINTER");
+    else printf("Unknown pointer");
+    printf(" (%p)", v);
+}
+
+void printty(int v) {
+    if (v == incA) printf("incA");
+    else if (v == incB) printf("incB");
+    else if (v == incC) printf("incC");
+    else if (v == M) printf("M");
+    else if (v == N) printf("N");
+    else if (v == lda) printf("lda");
+    else if (v == UNUSED_INT) printf("UNUSED_INT");
+    else printf("Unknown int");
+    printf(" (%d)", v);
+}
+void printty(char v) {
+    if (v == DEFAULT_LAYOUT) {
+		printf("DEFAULT_LAYOUT (%x)", v);
+		return;
+	}
+    if (v == UNUSED_TRANS) {
+		printf("UNUSED_TRANS (%c)", v);
+		return;
+	}
+	for (auto t : {'N', 'n', 'T', 't'}) {
+		if (v == t) {
+			printf("'%c'", v);
+			return;
+		}
+	}
+    printf("Unknown char ('%c'=%d)", v, v);
+}
+void printty(double v) {
+    if (v == alpha) printf("alpha");
+    else if (v == beta) printf("beta");
+    else if (v == UNUSED_DOUBLE) printf("UNUSED_DOUBLE");
+    else printf("Unknown double");
+    printf(" (%f)", v);
+}
+
+void printcall(BlasCall rcall) {
+  switch (rcall.type) {
+      case CallType::GEMV:
+		printf("GEMV(layout=");
+		printty(rcall.layout);
+		printf(", trans=");
+		printty(rcall.targ1);
+		printf(", M=");
+		printty(rcall.iarg1);
+		printf(", N=");
+		printty(rcall.iarg2);
+		printf(", alpha=");
+		printty(rcall.farg1);
+		printf(", A=");
+		printty(rcall.pin_arg1);
+		printf(", lda=");
+		printty(rcall.iarg4);
+		printf(", X=");
+		printty(rcall.pin_arg2);
+		printf(", incx=");
+		printty(rcall.iarg5);
+		printf(", beta=");
+		printty(rcall.farg2);
+		printf(", Y=");
+		printty(rcall.pout_arg1);
+		printf(", incy=");
+		printty(rcall.iarg6);
+		printf(")");
+		return;
+      case CallType::GEMM:
+		printf("GEMM(layout=");
+		printty(rcall.layout);
+		printf(", transA=");
+		printty(rcall.targ1);
+		printf(", transB=");
+		printty(rcall.targ2);
+		printf(", M=");
+		printty(rcall.iarg1);
+		printf(", N=");
+		printty(rcall.iarg2);
+		printf(", K=");
+		printty(rcall.iarg3);
+		printf(", alpha=");
+		printty(rcall.farg1);
+		printf(", A=");
+		printty(rcall.pin_arg1);
+		printf(", lda=");
+		printty(rcall.iarg4);
+		printf(", B=");
+		printty(rcall.pin_arg2);
+		printf(", ldb=");
+		printty(rcall.iarg5);
+		printf(", beta=");
+		printty(rcall.farg2);
+		printf(", C=");
+		printty(rcall.pout_arg1);
+		printf(", ldc=");
+		printty(rcall.iarg6);
+		printf(")");
+		return;
+      case CallType::SCAL:
+		printf("SCAL(N=");
+		printty(rcall.iarg1);
+		printf(", alpha=");
+		printty(rcall.farg1);
+		printf(", X=");
+		printty(rcall.pout_arg1);
+		printf(", incX=");
+		printty(rcall.iarg4);
+		printf(")");
+		return;
+      case CallType::GER:
+		printf("GER(layout=");
+		printty(rcall.layout);
+		printf(", M=");
+		printty(rcall.iarg1);
+		printf(", N=");
+		printty(rcall.iarg2);
+		printf(", alpha=");
+		printty(rcall.farg1);
+		printf(", X=");
+		printty(rcall.pin_arg1);
+		printf(", incX=");
+		printty(rcall.iarg4);
+		printf(", Y=");
+		printty(rcall.pin_arg2);
+		printf(", incY=");
+		printty(rcall.iarg5);
+		printf(", A=");
+		printty(rcall.pin_arg2);
+		printf(", lda=");
+		printty(rcall.iarg6);
+		printf(")");
+      case CallType::COPY:
+		printf("COPY(N=");
+		printty(rcall.iarg1);
+		printf(", X=");
+		printty(rcall.pin_arg1);
+		printf(", incX=");
+		printty(rcall.iarg4);
+		printf(", Y=");
+		printty(rcall.pout_arg1);
+		printf(", incY=");
+		printty(rcall.iarg5);
+		printf(")");
+      default: printf("UNKNOWN CALL (%d)", (int)rcall.type); return;
     }
 }
 
-std::string astr(void* v) {
-    if (v == A) return "A (" + std::to_string((double*)v) + ")";
-    if (v == dA) return "dA (" + std::to_string(v) + ")";
-    if (v == B) return "B (" + std::to_string(v) + ")";
-    if (v == dB) return "dB (" + std::to_string(v) + ")";
-    if (v == C) return "C (" + std::to_string(v) + ")";
-    if (v == dC) return "dC (" + std::to_string(v) + ")";
-    return "Unknown pointer (" + std::to_string(v) + ")";
-}
-
-std::string astr(int v) {
-    if (v == incA) return "incA (" + std::to_string(v) + ")";
-    if (v == incB) return "incB (" + std::to_string(v) + ")";
-    if (v == incC) return "incC (" + std::to_string(v) + ")";
-    if (v == M) return "M (" + std::to_string(v) + ")";
-    if (v == N) return "N (" + std::to_string(v) + ")";
-    if (v == lda) return "lda (" + std::to_string(v) + ")";
-    if (v == UNUSED_INT) return "UNUSED_INT (" + std::to_string(v) + ")";
-    return "Unknown int (" + std::to_string(v) + ")";
-}
-std::string astr(double v) {
-    if (v == alpha) return "alpha (" + std::to_string(v) + ")";
-    if (v == beta) return "beta (" + std::to_string(v) + ")";
-    return "Unknown double (" + std::to_string(v) + ")";
+void printTrace(const vector<BlasCall> &tr, std::string prefix="") {
+	printf("%sPrimal:\n", prefix.c_str());
+	bool reverse = false;
+	for (size_t i=0; i<tr.size(); i++) {
+		if (tr[i].inDerivative) {
+			if (!reverse) {
+				printf("%sReverse:\n", prefix.c_str());
+				reverse = true;
+			}
+		} else
+			assert(!reverse);
+		printf("%s  %zu:\t", prefix.c_str(), i);
+		printcall(tr[i]);
+		printf("\n");
+	}
 }
 
 template<typename T>
-void assert_eq(std::string scope, std::string varName, int i, T expected, T real) {
-    std::cerr << "Checking test: " << scope << " call: " << i << ", var " << varName << ":\n";
+void assert_eq(std::string scope, std::string varName, int i, T expected, T real, BlasCall texpected, BlasCall rcall) {
     if (expected == real) return;
-    std::cerr << "Failure on test: " << scope << " call: " << i << ", var " << varName << ":\n";
-    std::cerr << "  found, " << astr(expected) << " expected " << astr(real) << "\n";
+    printf("Failure on call %d var %s found ", i, varName.c_str());
+    printty(expected);
+    printf(" expected ");
+    printty(real);
+    printf("\n");
     assert(0);
     exit(1);
 }
 
 void check_equiv(std::string scope, int i, BlasCall expected, BlasCall real) {
-#define MAKEASSERT(name) assert_eq(scope, #name, i, expected.name, real.name);
+#define MAKEASSERT(name) assert_eq(scope, #name, i, expected.name, real.name, expected, real);
     MAKEASSERT(inDerivative)
     MAKEASSERT(type)
     MAKEASSERT(pout_arg1);
@@ -153,7 +378,8 @@ void check_equiv(std::string scope, int i, BlasCall expected, BlasCall real) {
     MAKEASSERT(iarg6);
 }
 
-std::vector<BlasCall> calls;
+vector<BlasCall> calls;
+vector<BlasCall> foundCalls;
 
 extern "C" {
 
@@ -184,8 +410,8 @@ void cblas_dgemm(char layout, char transA, char transB, int M, int N, int K, dou
 __attribute__((noinline))
 void cblas_dscal(int N, double alpha, double* X, int incX) {
     calls.push_back((BlasCall){inDerivative, CallType::SCAL,
-                                X, 0, 0,
-                                alpha, 3.14159,
+                                X, UNUSED_POINTER, UNUSED_POINTER,
+                                alpha, UNUSED_DOUBLE,
                                 UNUSED_TRANS,
                                 UNUSED_TRANS, UNUSED_TRANS,
                                 N, UNUSED_INT, UNUSED_INT, incX, UNUSED_INT, UNUSED_INT});
@@ -195,13 +421,23 @@ void cblas_dscal(int N, double alpha, double* X, int incX) {
 __attribute__((noinline))
 void cblas_dger(char layout, int M, int N, double alpha, double* X, int incX, double* Y, int incY, double* A, int lda) {
     calls.push_back((BlasCall){inDerivative, CallType::SCAL,
-                                X, 0, 0,
-                                alpha, 3.14159,
+                                A, X, Y,
+                                alpha, UNUSED_DOUBLE,
                                 UNUSED_TRANS,
                                 UNUSED_TRANS, UNUSED_TRANS,
-                                N, UNUSED_INT, UNUSED_INT, incX, UNUSED_INT, UNUSED_INT});
+                                M, N, UNUSED_INT, incX, incY, lda});
 }
 
+__attribute__((noinline))
+void cblas_dcopy(int N, double* X, int incX, double* Y, int incY) {
+
+    calls.push_back((BlasCall){inDerivative, CallType::COPY,
+                                Y, X, UNUSED_POINTER,
+                                alpha, UNUSED_DOUBLE,
+                                UNUSED_TRANS,
+                                UNUSED_TRANS, UNUSED_TRANS,
+                                N, UNUSED_INT, UNUSED_INT, incX, incY, UNUSED_INT});
+}
 }
 
 int enzyme_dup;
@@ -210,7 +446,7 @@ int enzyme_const;
 template<typename ...T>
 void __enzyme_autodiff(void*, T...);
 
-void my_dgemv(char layout, char trans, int M, int N, double alpha, double* A, int lda, double* X, int incx, double beta, double* Y, int incy) {
+void my_dgemv(char layout, char trans, int M, int N, double alpha, double* __restrict__ A, int lda, double* __restrict__ X, int incx, double beta, double* __restrict__ Y, int incy) {
     cblas_dgemv(layout, trans, M, N, alpha, A, lda, X, incx, beta, Y, incy);
     inDerivative = true;
 }
@@ -218,6 +454,29 @@ void my_dgemv(char layout, char trans, int M, int N, double alpha, double* A, in
 void init() {
     inDerivative = false;
     calls.clear();
+}
+
+void checkTest(std::string name) {
+
+        if (foundCalls.size() != calls.size()) {
+            printf("Test %s failed: Expected %zu calls, found %zu\n", name.c_str(), calls.size(), foundCalls.size());
+			printf("Expected:\n");
+			printTrace(calls, "  ");
+			printf("Found:\n");
+			printTrace(foundCalls, "  ");
+            assert(0 && "non-equal call count");
+            exit(1);
+        }
+		if (foundCalls != calls) {
+            printf("Test %s failed\n", name.c_str());
+			printf("Expected:\n");
+			printTrace(calls, "  ");
+			printf("Found:\n");
+			printTrace(foundCalls, "  ");
+		}
+        for (size_t i=0; i<calls.size(); i++) {
+            check_equiv(name, i, foundCalls[i], calls[i]);
+        }
 }
 
 int main() {
@@ -231,7 +490,7 @@ int main() {
     {
     assert(calls.size() == 1);
     assert(calls[0].inDerivative == false);
-    assert(calls[0].type == CallType::GEMM);
+    assert(calls[0].type == CallType::GEMV);
     assert(calls[0].pout_arg1 == C);
     assert(calls[0].pin_arg1 == A);
     assert(calls[0].pin_arg2 == B);
@@ -263,7 +522,7 @@ int main() {
                             enzyme_dup, C, dC,
                             enzyme_const, incC);
     {
-        auto foundCalls = calls;
+        foundCalls = calls;
         init();
 
         my_dgemv(DEFAULT_LAYOUT, transA, M, N, alpha, A, lda, B, incB, beta, C, incC);
@@ -275,11 +534,7 @@ int main() {
         cblas_dscal((transA == 'N' || transA == 'n') ? M : N,
                     beta, dC, incC);
 
-        assert(foundCalls.size() == calls.size());
-        for (size_t i=0; i<calls.size(); i++) {
-            check_equiv("GEMV active A, C", i, foundCalls[i], calls[i]);
-        }
-        assert(0 && "should not get here");
+		checkTest("GEMV active A, C");
     }
 
 
