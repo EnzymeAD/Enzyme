a75723eecd59908d8633e8a10aa4fa448b12ad70
William Moses <gh@wsmoses.com>
2023-10-16 15:11:28 -0400

    Fix memory errors (#1488)
    * Attempt fix of memory errors
    
    * Try again
    
    * fix mem issue

--- enzyme/Enzyme/Utils.cpp
+++ enzyme/Enzyme/Utils.cpp
@@ -640,7 +640,7 @@ void callMemcpyStridedBlas(llvm::IRBuilder<> &B, llvm::Module &M, BlasInfo blas,
                            llvm::ArrayRef<llvm::Value *> args,
                            llvm::Type *copy_retty,
                            llvm::ArrayRef<llvm::OperandBundleDef> bundles) {
-  auto copy_name = Twine(blas.prefix) + blas.floatType + "copy" + blas.suffix;
+  Twine copy_name = Twine(blas.prefix) + blas.floatType + "copy" + blas.suffix;
 
   SmallVector<Type *, 1> tys;
   for (auto arg : args)
@@ -655,7 +655,7 @@ void callMemcpyStridedBlas(llvm::IRBuilder<> &B, llvm::Module &M, BlasInfo blas,
 void callMemcpyStridedLapack(llvm::IRBuilder<> &B, llvm::Module &M,
                              BlasInfo blas, llvm::ArrayRef<llvm::Value *> args,
                              llvm::ArrayRef<llvm::OperandBundleDef> bundles) {
-  auto copy_name = Twine(blas.prefix) + blas.floatType + "lacpy" + blas.suffix;
+  Twine copy_name = Twine(blas.prefix) + blas.floatType + "lacpy" + blas.suffix;
 
   SmallVector<Type *, 1> tys;
   for (auto arg : args)
@@ -674,14 +674,14 @@ void callSPMVDiagUpdate(IRBuilder<> &B, Module &M, BlasInfo blas,
                         ArrayRef<OperandBundleDef> bundles, bool byRef,
                         bool julia_decl) {
   // add spmv diag update call if not already present
-  auto fnc_name = Twine("__enzyme_spmv_diag") + blas.floatType + blas.suffix;
+  auto fnc_name = "__enzyme_spmv_diag" + blas.floatType + blas.suffix;
 
   //  spmvDiagHelper(uplo, n, alpha, x, incx, ya, incy, APa)
   auto FDiagUpdateT = FunctionType::get(
       B.getVoidTy(),
       {BlasCT, BlasIT, BlasFPT, BlasPT, BlasIT, BlasPT, BlasIT, BlasPT}, false);
-  Function *F = cast<Function>(
-      M.getOrInsertFunction(fnc_name.str(), FDiagUpdateT).getCallee());
+  Function *F =
+      cast<Function>(M.getOrInsertFunction(fnc_name, FDiagUpdateT).getCallee());
 
   if (!F->empty()) {
     B.CreateCall(F, args, bundles);
