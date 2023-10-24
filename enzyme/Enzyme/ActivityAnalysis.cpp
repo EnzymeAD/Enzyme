ebbb7307263b357b9c3b6d94d82af522b84ee73f
Jacob Peng <jacobmpeng@gmail.com>
2023-10-24 17:28:26 -0400

    Add additional overwriting to LLVM activity analysis, attribute to metadata translation pipeline

--- enzyme/Enzyme/ActivityAnalysis.cpp
+++ enzyme/Enzyme/ActivityAnalysis.cpp
@@ -658,6 +658,18 @@ bool ActivityAnalyzer::isConstantInstruction(TypeResults const &TR,
     return false;
   }
 
+  /// Overwrite activity using metadata
+  if (hasMetadata(I, "enzyme_active") || hasMetadata(I, "enzyme_active_inst")) {
+    if (EnzymePrintActivity)
+      llvm::errs() << "[mlir] forced instruction to be active: " << *I << "\n";
+    return false;
+  } else if (hasMetadata(I, "enzyme_inactive") ||
+             hasMetadata(I, "enzyme_inactive_inst")) {
+    if (EnzymePrintActivity)
+      llvm::errs() << "[mlir] forced value to be constant: " << *I << "\n";
+    return true;
+  }
+
   if (notForAnalysis.count(I->getParent())) {
     if (EnzymePrintActivity)
       llvm::errs() << " constant instruction as dominates unreachable " << *I
@@ -1041,6 +1053,21 @@ bool ActivityAnalyzer::isConstantValue(TypeResults const &TR, Value *Val) {
                    << TR.intType(1, Val, /*errIfNotFound*/ false).str() << "\n";
     InsertConstantValue(TR, Val);
     return true;
+  }
+
+  /// Overwrite activity using metadata
+  if (auto *I = dyn_cast<Instruction>(Val)) {
+    if (hasMetadata(I, "enzyme_active") ||
+        hasMetadata(I, "enzyme_active_val")) {
+      if (EnzymePrintActivity)
+        llvm::errs() << "[mlir] forced value to be active: " << *Val << "\n";
+      return false;
+    } else if (hasMetadata(I, "enzyme_inactive") ||
+               hasMetadata(I, "enzyme_inactive_val")) {
+      if (EnzymePrintActivity)
+        llvm::errs() << "[mlir] forced value to be constant: " << *Val << "\n";
+      return true;
+    }
   }
 
 #if 0
