e4bcdc197fcdc377addaa7c1209ffa3c4ec59148
William S. Moses <gh@wsmoses.com>
2023-11-05 21:34:32 -0500

    fix

--- enzyme/Enzyme/Utils.cpp
+++ enzyme/Enzyme/Utils.cpp
@@ -932,14 +932,20 @@ getorInsertInnerProd(llvm::IRBuilder<> &B, llvm::Module &M, BlasInfo blas,
 
   {
     IRBuilder<> B1(entry);
-    Value *blasOne =
-        to_blas_callconv(B1, ConstantInt::get(IT, 1), byRef, cublas,
-                         julia_decl ? IT : nullptr, B1, "constant.one");
+    Value *blasOne = to_blas_callconv(B1, ConstantInt::get(IT, 1), byRef,
+                                      cublas, nullptr, B1, "constant.one");
+
+    if (blasOne->getType() != BlasIT)
+      blasOne = B1.CreatePointerCast(blasOne, BlasIT, "intcast.constant.one");
+
     Value *m = load_if_ref(B1, IT, blasm, byRef);
     Value *n = load_if_ref(B1, IT, blasn, byRef);
     Value *size = B1.CreateNUWMul(m, n, "mat.size");
     Value *blasSize = to_blas_callconv(
         B1, size, byRef, cublas, julia_decl ? IT : nullptr, B1, "mat.size");
+
+    if (blasSize->getType() != BlasIT)
+      blasSize = B1.CreatePointerCast(blasSize, BlasIT, "intcast.mat.size");
     B1.CreateCondBr(B1.CreateICmpEQ(size, ConstantInt::get(IT, 0)), end, init);
 
     IRBuilder<> B2(init);
