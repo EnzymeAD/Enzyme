fac4995e18e1b27382cb690308f7c53015fae346
Manuel Drehwald <git@manuel.drehwald.info>
2024-01-27 05:08:38 -0500

    remove boundscheck from safe rust

--- enzyme/bench/gen_rs.py
+++ enzyme/bench/gen_rs.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def create_all_f(out_path, n_funcs, n_lines):
     f_out = open(out_path, 'w+')
-    f_out.write('#![feature(rustc_attrs)]\n')
-    f_out.write('#![feature(autodiff)]\n')
+    f_out.write('//#![feature(rustc_attrs)]\n')
+    f_out.write('//#![feature(autodiff)]\n')
     f_out.write('use std::hint::black_box;\n\n')
     f_out.close()
     for i in range(n_funcs):
-        create_f(out_path, i, n_lines, max(10,i*n_lines))
+        create_f(out_path, i, n_lines, max(10,i*n_lines), n_funcs)
     f_out = open(out_path, 'a+')
     f_out.write('#[autodiff(df, Reverse, Duplicated)]\n')
     f_out.write('fn f(vec: &mut [f32]) {\n')
@@ -17,11 +17,12 @@ def create_all_f(out_path, n_funcs, n_lines):
         f_out.write('    f_{}(vec);\n'.format(i))
     f_out.write('}\n\n')
 
-def create_f(out_path, block, n_lines, offset):
+def create_f(out_path, block, n_lines, offset, n_funcs):
     f_out = open(out_path, 'a+')
     ops = ['+', '-', '*'] #, '/' too likely to cause issues
     
     f_out.write('fn f_{}(vec: &mut [f32]) {{\n'.format(block))
+    f_out.write('    assert!(vec.len() > {});\n'.format(n_lines * n_funcs))
     for i in range(n_lines):
         str_ = '    vec[{}] = '.format(i+offset)
         for j in range(2):
@@ -39,8 +40,8 @@ def create_main(file_mono, n_funcs, n_lines):
     f_out.write('    let mut vec = vec![0.0; {}];\n'.format(n_funcs*n_lines))
     f_out.write('    let mut dvec = vec![0.0; {}];\n'.format(n_funcs*n_lines))
     f_out.write('    black_box(vec[0..10].fill(3.14));\n')
-    f_out.write('    // f(&mut vec);\n')
-    f_out.write('    df(&mut vec, &mut dvec);\n')
+    f_out.write('    f(&mut vec);\n')
+    f_out.write('    // df(&mut vec, &mut dvec);\n')
     f_out.write('    black_box(vec);\n')
     f_out.write('    black_box(dvec);\n')
     f_out.write('}\n')
