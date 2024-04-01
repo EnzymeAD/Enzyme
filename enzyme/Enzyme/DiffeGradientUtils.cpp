1dc150c734e386badfea6f3b01d9053b058ec9a7
William S. Moses <gh@wsmoses.com>
2024-04-01 12:53:03 -0400

    fix analyzer capture

--- enzyme/Enzyme/DiffeGradientUtils.cpp
+++ enzyme/Enzyme/DiffeGradientUtils.cpp
@@ -496,7 +496,7 @@ DiffeGradientUtils::addToDiffe(Value *val, Value *dif, IRBuilder<> &BuilderM,
       ss << "}\n";
       if (CustomErrorHandler) {
         CustomErrorHandler(ss.str().c_str(), wrap(val), ErrorType::NoType,
-                           &TR.analyzer, nullptr, wrap(&BuilderM));
+                           TR.analyzer, nullptr, wrap(&BuilderM));
         return addedSelects;
       } else {
         TR.dump(ss);
@@ -534,7 +534,7 @@ DiffeGradientUtils::addToDiffe(Value *val, Value *dif, IRBuilder<> &BuilderM,
          << "\n";
       if (CustomErrorHandler) {
         CustomErrorHandler(ss.str().c_str(), wrap(val), ErrorType::NoType,
-                           &TR.analyzer, nullptr, wrap(&BuilderM));
+                           TR.analyzer, nullptr, wrap(&BuilderM));
         return addedSelects;
       } else {
         DebugLoc loc;
