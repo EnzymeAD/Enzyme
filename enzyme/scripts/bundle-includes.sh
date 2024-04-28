fa2104aaddf7b808cd423e1df4e265a98c43ca8a
William S. Moses <gh@wsmoses.com>
2024-04-28 16:05:23 -0400

    Bazel more bundle include fixes

--- enzyme/scripts/bundle-includes.sh
+++ enzyme/scripts/bundle-includes.sh
@@ -11,7 +11,7 @@ mkdir -p "$(dirname "$OUTPUT_FILE")"
 
 echo > "$OUTPUT_FILE"
 echo "const char* include_headers[][2] = {" >> "$OUTPUT_FILE"
-for FILE in $(find $INPUT_DIR -type f); do
+for FILE in $(find -L $INPUT_DIR -type f); do
     echo $FILE
     INTERNAL_FILENAME=$(echo $FILE | sed "s|$INPUT_DIR|\/enzymeroot\/|")
     echo $INTERNAL_FILENAME
