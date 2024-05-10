e96ccd2ecd315656d18a548c479904859391caea
William Moses <gh@wsmoses.com>
2024-05-10 11:32:28 -0700

    Fixup diff malloc fb (#1872)
    * Diffuse malloc fb
    
    * fixup
    
    * fixup

--- enzyme/Enzyme/AdjointGenerator.h
+++ enzyme/Enzyme/AdjointGenerator.h
@@ -6288,134 +6288,15 @@ public:
       return;
 
     bool useConstantFallback =
-        gutils->isConstantInstruction(&call) &&
-        (gutils->isConstantValue(&call) || !shadowReturnUsed);
-    if (useConstantFallback && Mode != DerivativeMode::ForwardMode &&
-        Mode != DerivativeMode::ForwardModeError) {
-      // if there is an escaping allocation, which is deduced needed in
-      // reverse pass, we need to do the recursive procedure to perform the
-      // free.
-
-      // First test if the return is a potential pointer and needed for the
-      // reverse pass
-      bool escapingNeededAllocation = false;
-
-      if (!isNoEscapingAllocation(&call)) {
-        escapingNeededAllocation = EnzymeGlobalActivity;
-
-        std::map<UsageKey, bool> CacheResults;
-        for (auto pair : gutils->knownRecomputeHeuristic) {
-          if (!pair.second || gutils->unnecessaryIntermediates.count(
-                                  cast<Instruction>(pair.first))) {
-            CacheResults[UsageKey(pair.first, QueryType::Primal)] = false;
-          }
-        }
-
-        if (!escapingNeededAllocation &&
-            !(EnzymeJuliaAddrLoad && isSpecialPtr(call.getType()))) {
-          if (TR.query(&call)[{-1}].isPossiblePointer()) {
-            auto found = gutils->knownRecomputeHeuristic.find(&call);
-            if (found != gutils->knownRecomputeHeuristic.end()) {
-              if (!found->second) {
-                CacheResults.erase(UsageKey(&call, QueryType::Primal));
-                escapingNeededAllocation =
-                    DifferentialUseAnalysis::is_value_needed_in_reverse<
-                        QueryType::Primal>(gutils, &call,
-                                           DerivativeMode::ReverseModeGradient,
-                                           CacheResults, oldUnreachable);
-              }
-            } else {
-              escapingNeededAllocation =
-                  DifferentialUseAnalysis::is_value_needed_in_reverse<
-                      QueryType::Primal>(gutils, &call,
-                                         DerivativeMode::ReverseModeGradient,
-                                         CacheResults, oldUnreachable);
-            }
-          }
-        }
-
-        // Next test if any allocation could be stored into one of the
-        // arguments.
-        if (!escapingNeededAllocation)
-#if LLVM_VERSION_MAJOR >= 14
-          for (unsigned i = 0; i < call.arg_size(); ++i)
-#else
-          for (unsigned i = 0; i < call.getNumArgOperands(); ++i)
-#endif
-          {
-            Value *a = call.getOperand(i);
-
-            if (EnzymeJuliaAddrLoad && isSpecialPtr(a->getType()))
-              continue;
-
-            auto vd = TR.query(a);
-            if (!vd[{-1}].isPossiblePointer())
-              continue;
-
-            if (!vd[{-1, -1}].isPossiblePointer())
-              continue;
-
-            if (isReadOnly(&call, i))
-              continue;
-
-            // An allocation could only be needed in the reverse pass if it
-            // escapes into an argument. However, is the parameter by which it
-            // escapes could capture the pointer, the rest of Enzyme's caching
-            // mechanisms cannot assume that the allocation itself is
-            // reloadable, since it may have been captured and overwritten
-            // elsewhere.
-            // TODO: this justification will need revisiting in the future as
-            // the caching algorithm becomes increasingly sophisticated.
-            if (!isNoCapture(&call, i))
-              continue;
-
-            escapingNeededAllocation = true;
-          }
+        DifferentialUseAnalysis::callShouldNotUseDerivative(gutils, call);
+    if (!useConstantFallback) {
+      if (gutils->isConstantInstruction(&call) &&
+          gutils->isConstantValue(&call)) {
+        EmitWarning("ConstnatFallback", call,
+                    "Call was deduced inactive but still doing differential "
+                    "rewrite as it may escape an allocation",
+                    call);
       }
-
-      // If desired this can become even more aggressive by looking through the
-      // called function for any allocations.
-      if (auto F = getFunctionFromCall(&call)) {
-        SmallVector<Function *, 1> todo = {F};
-        SmallPtrSet<Function *, 1> done;
-        bool seenAllocation = false;
-        while (todo.size() && !seenAllocation) {
-          auto cur = todo.pop_back_val();
-          if (done.count(cur))
-            continue;
-          done.insert(cur);
-          // assume empty functions allocate.
-          if (cur->empty()) {
-            // unless they are marked
-            if (isNoEscapingAllocation(cur))
-              continue;
-            seenAllocation = true;
-            break;
-          }
-          for (auto &BB : *cur)
-            for (auto &I : BB)
-              if (auto CB = dyn_cast<CallBase>(&I)) {
-                if (isNoEscapingAllocation(CB))
-                  continue;
-                if (isAllocationCall(CB, gutils->TLI)) {
-                  seenAllocation = true;
-                  goto finish;
-                }
-                if (auto F = getFunctionFromCall(CB)) {
-                  todo.push_back(F);
-                  continue;
-                }
-                // Conservatively assume indirect functions allocate.
-                seenAllocation = true;
-                goto finish;
-              }
-        finish:;
-        }
-        if (!seenAllocation)
-          escapingNeededAllocation = false;
-      }
-      if (escapingNeededAllocation)
-        useConstantFallback = false;
     }
     if (useConstantFallback) {
       if (!gutils->isConstantValue(&call)) {
