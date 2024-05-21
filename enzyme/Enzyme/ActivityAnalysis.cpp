cf7c81d428ea5b17d414916ce09ea2282b049d8d
Manuel Drehwald <git@manuel.drehwald.info>
2024-05-21 19:02:53 -0400

    Update ActivityAnalysis.cpp

--- enzyme/Enzyme/ActivityAnalysis.cpp
+++ enzyme/Enzyme/ActivityAnalysis.cpp
@@ -46,8 +46,8 @@
 
 #include "llvm/IR/InstIterator.h"
 
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/TimeProfiler.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include "llvm/IR/InlineAsm.h"
 
