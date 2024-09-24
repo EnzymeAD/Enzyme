88a76e6bdb14dacdfb8a21cb4bb35fd295156a0d
Harshitha Menon <gopalakrishn1@llnl.gov>
2024-09-23 21:27:46 -0700

    add floating-point check for the gradients

--- enzyme/Enzyme/Utils.cpp
+++ enzyme/Enzyme/Utils.cpp
@@ -98,6 +98,9 @@ llvm::cl::opt<bool> EnzymeRuntimeError(
     cl::desc("Emit Runtime errors instead of compile time ones"));
 }
 
+
+__attribute__((weak)) void enzyme_fp_check(double) {}
+
 void ZeroMemory(llvm::IRBuilder<> &Builder, llvm::Type *T, llvm::Value *obj,
                 bool isTape) {
   if (CustomZero) {
@@ -3025,6 +3028,15 @@ llvm::Value *SanitizeDerivatives(llvm::Value *val, llvm::Value *toset,
   if (EnzymeSanitizeDerivatives)
     return unwrap(EnzymeSanitizeDerivatives(wrap(val), wrap(toset),
                                             wrap(&BuilderM), wrap(mask)));
+
+  if (((Value*)toset)->getType()->Type::isDoubleTy()) {
+    std::string fp_check_fnname = "enzyme_fp_check";
+    Module &M = *BuilderM.GetInsertBlock()->getParent()->getParent();
+    if (M.getFunction(fp_check_fnname)){
+      Function *F = cast<Function>(M.getFunction(fp_check_fnname));
+      BuilderM.CreateCall(F, toset);
+    }
+  }
   return toset;
 }
 
