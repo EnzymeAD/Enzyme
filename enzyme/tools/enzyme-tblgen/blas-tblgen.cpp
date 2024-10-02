2ae5ba6ad140bac8b2ceb0c10fab83be7064a7be
Billy Moses <gh@wsmoses.com>
2024-10-02 18:33:10 +0000

    fix

--- enzyme/tools/enzyme-tblgen/blas-tblgen.cpp
+++ enzyme/tools/enzyme-tblgen/blas-tblgen.cpp
@@ -1457,7 +1457,7 @@ void rev_call_args(bool forward, Twine argName, const TGPattern &pattern,
   }
 }
 
-void emit_tmp_free(Record *Def, raw_ostream &os, StringRef builder) {
+void emit_tmp_free(const Record *Def, raw_ostream &os, StringRef builder) {
   const auto args = Def->getValueAsListOfStrings("args");
   // allocating tmp variables is optional, return if not required
   if (args.size() == 0)
