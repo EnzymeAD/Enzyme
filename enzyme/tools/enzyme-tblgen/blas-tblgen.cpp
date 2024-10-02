18efd235f3ec69ba90551ab9c9fb7ca2d971ecdd
Billy Moses <gh@wsmoses.com>
2024-10-02 17:33:17 +0000

    fix

--- enzyme/tools/enzyme-tblgen/blas-tblgen.cpp
+++ enzyme/tools/enzyme-tblgen/blas-tblgen.cpp
@@ -1467,7 +1467,7 @@ void emit_tmp_free(Record *Def, raw_ostream &os, StringRef builder) {
   os << "    CreateDealloc(" << builder << ", true_" << allocName << ");\n";
 }
 
-void emit_tmp_creation(Record *Def, raw_ostream &os, StringRef builder) {
+void emit_tmp_creation(const Record *Def, raw_ostream &os, StringRef builder) {
   const auto args = Def->getValueAsListOfStrings("args");
   // allocating tmp variables is optional, return if not required
   if (args.size() == 0)
