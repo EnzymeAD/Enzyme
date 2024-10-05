ac0c573b30449b6f2b1ffd558ed3328e6db6e769
William Moses <gh@wsmoses.com>
2024-10-05 15:30:15 -0500

    MLIR: improved tensor type error messages (#2103)
    * MLIR: improved tensor type error messages
    
    * fmt

--- enzyme/Enzyme/MLIR/Implementations/BuiltinAutoDiffTypeInterfaceImpl.cpp
+++ enzyme/Enzyme/MLIR/Implementations/BuiltinAutoDiffTypeInterfaceImpl.cpp
@@ -77,6 +77,13 @@ public:
         return builder.create<arith::ConstantOp>(loc, tenType, attr);
       }
     }
+    if (auto IT = dyn_cast<IntegerType>(ET)) {
+      APInt apvalue(IT.getWidth(), 0);
+      auto attr = DenseElementsAttr::get(tenType, apvalue);
+      return builder.create<arith::ConstantOp>(loc, tenType, attr);
+    }
+    llvm::errs() << " cannot create null value of tensor type: " << tenType
+                 << "\n";
     assert(0);
     return nullptr;
   }
