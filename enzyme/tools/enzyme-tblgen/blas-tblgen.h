3fe425e524109188a9aae96d5c54e1f343f55f9c
Matt Bolitho <matt.bolitho.software@gmail.com>
2024-10-17 21:22:04 +0100

    Fixes ssize_t/size_t warnings

--- enzyme/tools/enzyme-tblgen/blas-tblgen.h
+++ enzyme/tools/enzyme-tblgen/blas-tblgen.h
@@ -7,5 +7,5 @@ void emitBlasDerivatives(const llvm::RecordKeeper &RK, llvm::raw_ostream &os);
 bool hasDiffeRet(llvm::Init *resultTree);
 bool hasAdjoint(const TGPattern &pattern, llvm::Init *resultTree,
                 llvm::StringRef argName);
-llvm::SmallString<80> ValueType_helper(const TGPattern &pattern, ssize_t actPos,
+llvm::SmallString<80> ValueType_helper(const TGPattern &pattern, size_t actPos,
                                        llvm::DagInit *ruleDag);
