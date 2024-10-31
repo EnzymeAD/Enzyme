a273bdf66e9c01f1f8ed773796eea7867edddc4c
Jed Brown <jed@jedbrown.org>
2024-10-31 12:33:20 -0600

    bench fft: more idiomatic safe Rust

--- enzyme/benchmarks/ReverseMode/fft/src/safe.rs
+++ enzyme/benchmarks/ReverseMode/fft/src/safe.rs
@@ -1,19 +1,23 @@
-use std::slice;
-use std::f64::consts::PI;
 use std::autodiff::autodiff;
+use std::f64::consts::PI;
+use std::slice;
 
 fn bitreversal_perm<T>(data: &mut [T]) {
     let len = data.len() / 2;
     let mut j = 1;
 
     let mut i = 1;
-    while i < 2*len {
+    while i < 2 * len {
         if j > i {
             //dbg!(&i, &j);
             //data.swap(j-1, i-1);
             //data.swap(j, i);
-            unsafe {data.swap_unchecked(j-1, i-1);}
-            unsafe {data.swap_unchecked(j, i);}
+            unsafe {
+                data.swap_unchecked(j - 1, i - 1);
+            }
+            unsafe {
+                data.swap_unchecked(j, i);
+            }
         }
 
         let mut m = len;
@@ -27,14 +31,15 @@ fn bitreversal_perm<T>(data: &mut [T]) {
     }
 }
 
-fn radix2(data: &mut [f64], i_sign: f64, n: usize) {
+fn radix2(data: &mut [f64], i_sign: f64) {
+    let n = data.len() / 2;
     if n == 1 {
         return;
     }
 
-    let (a,b) = data.split_at_mut(n);
-    radix2(a, i_sign, n/2);
-    radix2(b, i_sign, n/2);
+    let (a, b) = data.split_at_mut(n);
+    radix2(a, i_sign);
+    radix2(b, i_sign);
 
     let wtemp = i_sign * (PI / n as f64).sin();
     let wpi = -i_sign * (2.0 * PI / n as f64).sin();
@@ -42,22 +47,19 @@ fn radix2(data: &mut [f64], i_sign: f64, n: usize) {
     let mut wr = 1.0;
     let mut wi = 0.0;
 
-    assert_eq!(data.len(), 2*n);
-    for i in (0..n).step_by(2)  {
-        let in_n = i + n;
-        let tempr = data[in_n] * wr - data[in_n + 1] * wi;
-        let tempi = data[in_n] * wi + data[in_n + 1] * wr;
+    for i in (0..n).step_by(2) {
+        let tempr = b[i] * wr - b[i + 1] * wi;
+        let tempi = b[i] * wi + b[i + 1] * wr;
 
-        data[in_n] = data[i] - tempr;
-        data[in_n + 1] = data[i + 1] - tempi;
-        data[i] += tempr;
-        data[i + 1] += tempi;
+        b[i] = a[i] - tempr;
+        b[i + 1] = a[i + 1] - tempi;
+        a[i] += tempr;
+        a[i + 1] += tempi;
 
         let wtemp_new = wr;
         wr += wr * wpr - wi * wpi;
         wi += wi * wpr + wtemp_new * wpi;
     }
-
 }
 
 fn rescale(data: &mut [f64], scale: usize) {
@@ -69,12 +71,12 @@ fn rescale(data: &mut [f64], scale: usize) {
 
 fn fft(data: &mut [f64]) {
     bitreversal_perm(data);
-    radix2(data, 1.0, data.len() / 2);
+    radix2(data, 1.0);
 }
 
 fn ifft(data: &mut [f64]) {
     bitreversal_perm(data);
-    radix2(data, -1.0, data.len() / 2);
+    radix2(data, -1.0);
     rescale(data, data.len() / 2);
 }
 
@@ -86,15 +88,14 @@ pub fn foobar(data: &mut [f64]) {
 
 #[no_mangle]
 pub extern "C" fn rust_dfoobar(n: usize, data: *mut f64, ddata: *mut f64) {
-    
     let (data, ddata) = unsafe {
         (
             slice::from_raw_parts_mut(data, n * 2),
-            slice::from_raw_parts_mut(ddata, n * 2)
+            slice::from_raw_parts_mut(ddata, n * 2),
         )
     };
 
-    unsafe {dfoobar(data, ddata)};
+    unsafe { dfoobar(data, ddata) };
 }
 
 #[no_mangle]
