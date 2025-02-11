10c3e1fbd26a1806b43923e476777b308bf513f7
Vimarsh Sathia <vsathia2@illinois.edu>
2025-02-10 18:45:38 -0600

    update opaque ptr

--- enzyme/Enzyme/Herbie.cpp
+++ enzyme/Enzyme/Herbie.cpp
@@ -4086,7 +4086,7 @@ bool accuracyGreedySolver(
     double bestAccuracyCost = std::numeric_limits<double>::infinity();
     InstructionCost bestCandidateComputationCost;
 
-    for (auto &candidate : enumerate(AO.candidates)) {
+    for (const auto &candidate : enumerate(AO.candidates)) {
       size_t i = candidate.index();
       auto candCompCost = AO.getCompCostDelta(i);
       auto candAccCost = AO.getAccCostDelta(i);
@@ -4245,7 +4245,7 @@ bool accuracyDPSolver(
         InstructionCost currCompCost = pair.first;
         double currAccCost = pair.second;
 
-        for (auto &candidate : enumerate(AO.candidates)) {
+        for (const auto &candidate : enumerate(AO.candidates)) {
           size_t i = candidate.index();
           auto candCompCost = AO.getCompCostDelta(i);
           auto candAccCost = AO.getAccCostDelta(i);
@@ -4348,7 +4348,7 @@ bool accuracyDPSolver(
         InstructionCost currCompCost = pair.first;
         double currAccCost = pair.second;
 
-        for (auto &candidate : enumerate(ACC.candidates)) {
+        for (const auto &candidate : enumerate(ACC.candidates)) {
           size_t i = candidate.index();
           auto candCompCost =
               ACC.getAdjustedCompCostDelta(i, costToSolutionMap[currCompCost]);
@@ -4744,13 +4744,15 @@ B2:
               llvm::errs() << "Registered FPNode for " << dtype
                            << " constant: " << value << "\n";
           } else if (auto GV = dyn_cast<GlobalVariable>(operand)) {
+            Type* elemType = GV->getValueType();
+          
             assert(
-                GV->getType()->getPointerElementType()->isFloatingPointTy() &&
+                elemType->isFloatingPointTy() &&
                 "Global variable is not floating point type");
             std::string dtype;
-            if (GV->getType()->getPointerElementType()->isFloatTy()) {
+            if (elemType->isFloatTy()) {
               dtype = "f32";
-            } else if (GV->getType()->getPointerElementType()->isDoubleTy()) {
+            } else if (elemType->isDoubleTy()) {
               dtype = "f64";
             } else {
               llvm_unreachable(
@@ -4853,7 +4855,7 @@ B2:
         auto operands =
             isa<CallInst>(I2) ? cast<CallInst>(I2)->args() : I2->operands();
 
-        for (auto &operand_ : enumerate(operands)) {
+        for (const auto &operand_ : enumerate(operands)) {
           auto &operand = operand_.value();
           auto i = operand_.index();
           if (!Poseidonable(*operand)) {
