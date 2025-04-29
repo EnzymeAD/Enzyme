0c8559cec047ed0ae025dedaf03ce3eee482eecb
William S. Moses <gh@wsmoses.com>
2025-04-28 19:34:36 -0500

    Fix and test LLVM 19/20

--- enzyme/Enzyme/FunctionUtils.cpp
+++ enzyme/Enzyme/FunctionUtils.cpp
@@ -378,7 +378,7 @@ void RecursivelyReplaceAddressSpace(Value *AI, Value *rep, bool legal) {
           continue;
         }
       } else {
-        IRBuilder<> B(P->getParent()->getFirstNonPHIOrDbgOrLifetime());
+        IRBuilder<> B(&(*P->getParent()->getFirstNonPHIOrDbgOrLifetime()));
         auto nP = B.CreatePHI(rep->getType(), P->getNumOperands());
         for (size_t i = 0; i < NumOperands; i++) {
           nP->addIncoming(replacedOperands[i], P->getIncomingBlock(i));
