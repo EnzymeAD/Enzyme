082186d1ee125806e3910bca43ad27522948b72d
Paul Berg <9824244+Pangoraw@users.noreply.github.com>
2025-06-20 13:25:46 +0200

    fmt

--- enzyme/Enzyme/MLIR/Passes/RemovalUtils.cpp
+++ enzyme/Enzyme/MLIR/Passes/RemovalUtils.cpp
@@ -29,9 +29,9 @@ struct Node {
     OP,
   } type;
 
-  Node(Operation *O) : O(O), type(OP) {};
-  Node(Value V) : V(V), type(VAL) {};
-  Node() : type(NONE) {};
+  Node(Operation *O) : O(O), type(OP){};
+  Node(Value V) : V(V), type(VAL){};
+  Node() : type(NONE){};
 
   bool operator<(const Node N) const {
     if (type != N.type)
