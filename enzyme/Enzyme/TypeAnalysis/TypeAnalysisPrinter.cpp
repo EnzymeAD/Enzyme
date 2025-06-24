7748c31ff9ebd26d5d60ba06e515ce6ccf702944
Karan Janthe <86996507+KMJ-007@users.noreply.github.com>
2025-06-24 06:24:35 +0530

    Add C API for setting type-analysis-func via EnzymeSetCLString (#2356)
    * fixing linker error
    
    * adding PrintTAFn flag
    
    * Revert "adding PrintTAFn flag"
    
    This reverts commit 62468239dd6ba127a6a2ed9a6c001f503e5ea8a8.
    
    * formatting
    
    Signed-off-by: Karan Janthe <karanjanthe@gmail.com>
    
    ---------
    
    Signed-off-by: Karan Janthe <karanjanthe@gmail.com>

--- enzyme/Enzyme/TypeAnalysis/TypeAnalysisPrinter.cpp
+++ enzyme/Enzyme/TypeAnalysis/TypeAnalysisPrinter.cpp
@@ -64,15 +64,17 @@ using namespace llvm;
 #endif
 #define DEBUG_TYPE "type-analysis-results"
 
+extern "C" {
 /// Function ActivityAnalysis will be starting its run from
 llvm::cl::opt<std::string>
-    FunctionToAnalyze("type-analysis-func", cl::init(""), cl::Hidden,
-                      cl::desc("Which function to analyze/print"));
+    EnzymeFunctionToAnalyze("type-analysis-func", cl::init(""), cl::Hidden,
+                            cl::desc("Which function to analyze/print"));
+}
 
 namespace {
 bool printTypeAnalyses(llvm::Function &F) {
 
-  if (F.getName() != FunctionToAnalyze)
+  if (F.getName() != EnzymeFunctionToAnalyze)
     return /*changed*/ false;
 
   FnTypeInfo type_args(&F);
