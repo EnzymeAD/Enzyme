9b1793afc6225491d7a45ae359c76bfd04e36256
Axel Huebl <axel.huebl@plasma.ninja>
2025-07-07 12:33:30 -0700

    Skip `-O0`
    Reverse Mode has incorrect result.

--- enzyme/test/Integration/ReverseMode/stl_list.cpp
+++ enzyme/test/Integration/ReverseMode/stl_list.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang++ -std=c++11 -fno-exceptions -ffast-math -O0 %s -S -emit-llvm -o - | %opt - %OPloadEnzyme %enzyme -S | %lli -
+// FIXME: -O0 fails reverse mode (wrong result) https://github.com/EnzymeAD/Enzyme/pull/2370#issuecomment-3046307237
 // RUN: %clang++ -std=c++11 -fno-exceptions -ffast-math -O1 %s -S -emit-llvm -o - | %opt - %OPloadEnzyme %enzyme -S | %lli -
 // RUN: %clang++ -std=c++11 -fno-exceptions -ffast-math -O2 %s -S -emit-llvm -o - | %opt - %OPloadEnzyme %enzyme -S | %lli -
 // RUN: %clang++ -std=c++11 -fno-exceptions -ffast-math -O3 %s -S -emit-llvm -o - | %opt - %OPloadEnzyme %enzyme -S | %lli -
@@ -42,7 +42,7 @@ void test_reverse_list() {
         std::list<double> dvals = {1.0, 1.0, 1.0};
 
         double ret = __enzyme_autodiff((void*)test_simple_list, enzyme_dup, vals, dvals);
-        //APPROX_EQ( ret, 12., 1e-10);  // TODO: FAILS
+        APPROX_EQ( ret, 12., 1e-10);
     }
 }
 
