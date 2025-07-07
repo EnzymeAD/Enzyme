e063006eb7aab4d253b8b993358bdee52d590c80
Axel Huebl <axel.huebl@plasma.ninja>
2025-07-07 12:48:17 -0700

    Add `test_modify_list`
    Currently fails.

--- enzyme/test/Integration/ReverseMode/stl_list.cpp
+++ enzyme/test/Integration/ReverseMode/stl_list.cpp
@@ -8,10 +8,10 @@
 #include <list>
 
 
-template<typename T>
-extern double __enzyme_fwddiff(void*, int, T&, T&);
-template<typename T>
-extern double __enzyme_autodiff(void*, int, T&, T&);
+template<typename ...T>
+extern double __enzyme_fwddiff(void*, T...);
+template<typename ...T>
+extern double __enzyme_autodiff(void*, T...);
 
 
 double test_iterate_list(std::list<double>& vals) {
@@ -23,7 +23,24 @@ double test_iterate_list(std::list<double>& vals) {
     return result;
 }
 
+struct S {
+    S(double r) : x(r) {};
+    double x = 0.0;
+};
+
+double test_modify_list(std::list<S> vals, double x) {
+    vals.front().x = x;
+
+    // iterate over list
+    double result = 0.0;
+    for (const auto& val : vals) {
+        result += val.x * val.x;
+    }
+    return result;
+}
+
 void test_forward_list() {
+    // diff all values of list
     {
         std::list<double> vals = {1.0, 2.0, 3.0};
         std::list<double> dvals = {1.0, 1.0, 1.0};
@@ -31,15 +48,36 @@ void test_forward_list() {
         double ret = __enzyme_fwddiff((void*)test_iterate_list, enzyme_dup, vals, dvals);
         APPROX_EQ(ret, 12., 1e-10);
     }
+
+    // list is const, then first value set to active
+    {
+        std::list<S> vals = {S{1.0}, S{2.0}, S{3.0}};
+        double x = 3.0;
+        double dx = 1.0;
+
+        double ret = __enzyme_fwddiff((void*)test_modify_list, enzyme_const, vals, enzyme_dup, x, dx);
+        APPROX_EQ(ret, 6., 1e-10);
+    }
 }
 
 void test_reverse_list() {
+    // diff all values of list
     {
         std::list<double> vals = {1.0, 2.0, 3.0};
         std::list<double> dvals = {1.0, 1.0, 1.0};
 
         double ret = __enzyme_autodiff((void*)test_iterate_list, enzyme_dup, vals, dvals);
         APPROX_EQ(ret, 12., 1e-10);
+    }
+
+    // list is const, then first value set to active
+    {
+        std::list<S> vals = {S{1.0}, S{2.0}, S{3.0}};
+        double x = 3.0;
+        double dx = 1.0;
+
+        double ret = __enzyme_autodiff((void*)test_modify_list, enzyme_const, vals, enzyme_dup, x, dx);
+        APPROX_EQ(ret, 6., 1e-10);
     }
 }
 
