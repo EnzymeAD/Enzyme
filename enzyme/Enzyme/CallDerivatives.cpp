8c1a596158f6194f10e8ffd56a1660a61c54337e
William Moses <gh@wsmoses.com>
2025-07-15 21:05:05 -0500

    Fix error on unreachable users (#2383)
    * Fix error on unreachable users
    
    * fmt

--- enzyme/Enzyme/CallDerivatives.cpp
+++ enzyme/Enzyme/CallDerivatives.cpp
@@ -2751,7 +2751,7 @@ bool AdjointGenerator::handleKnownCallDerivatives(
             (Mode == DerivativeMode::ReverseModePrimal && forwardsShadow) ||
             (Mode == DerivativeMode::ReverseModeGradient && backwardsShadow)) {
           IRBuilder<> BuilderZ(gutils->getNewFromOriginal(&call));
-          for (int i = 0; i < gutils->getWidth(); i++) {
+          for (size_t i = 0; i < gutils->getWidth(); i++) {
             SmallVector<Value *, 1> iargs;
             bool first = true;
             for (auto &arg : call.args()) {
