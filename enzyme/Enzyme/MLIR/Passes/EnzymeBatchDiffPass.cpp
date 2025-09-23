cfb89868e0c10e323cdc89148eb70853b450dc68
Vimarsh Sathia <vsathia2@illinois.edu>
2025-09-23 13:59:04 -0500

    Move isReadOnly check to OperationUtils
    Create a new set of utility headers in OperationUtils for AD and
    activity analysis

--- enzyme/Enzyme/MLIR/Passes/EnzymeBatchDiffPass.cpp
+++ enzyme/Enzyme/MLIR/Passes/EnzymeBatchDiffPass.cpp
@@ -11,6 +11,7 @@
 #include "Passes/EnzymeBatchDiffPass.h"
 #include "Dialect/Ops.h"
 #include "Interfaces/GradientUtilsReverse.h"
+#include "Interfaces/OperationUtils.h"
 #include "PassDetails.h"
 #include "Passes/Passes.h"
 
@@ -90,37 +91,6 @@ Value extractArg(OpBuilder &builder, Location &loc, Type &argTy, Value &val,
   return out;
 }
 
-bool isReadOnly(Operation *op) {
-  // If the op has memory effects, try to characterize them to see if the op
-  // is trivially dead here.
-  if (auto effectInterface = dyn_cast<MemoryEffectOpInterface>(op)) {
-    // Check to see if this op either has no effects, or only allocates/reads
-    // memory.
-    SmallVector<MemoryEffects::EffectInstance, 1> effects;
-    effectInterface.getEffects(effects);
-    if (!llvm::all_of(effects, [op](const MemoryEffects::EffectInstance &it) {
-          return isa<MemoryEffects::Read>(it.getEffect());
-        })) {
-      return false;
-    }
-  }
-
-  bool isRecursiveContainer =
-      op->hasTrait<OpTrait::HasRecursiveMemoryEffects>() ||
-      isa<FunctionOpInterface>(op);
-  if (isRecursiveContainer) {
-    for (Region &region : op->getRegions()) {
-      for (auto &block : region) {
-        for (auto &nestedOp : block)
-          if (!isReadOnly(&nestedOp))
-            return false;
-      }
-    }
-  }
-
-  return true;
-}
-
 } // namespace batchutils
 } // namespace enzyme
 } // namespace mlir
@@ -145,7 +115,7 @@ struct BatchDiffPass : public enzyme::impl::BatchDiffPassBase<BatchDiffPass> {
       auto fnOp = cast<FunctionOpInterface>(symbolOp);
 
       // skip if fn isn't readonly(iterate through toplevel ops)
-      if (!batchutils::isReadOnly(fnOp)) {
+      if (!oputils::isReadOnly(fnOp)) {
         return mlir::WalkResult::skip();
       }
 
@@ -406,7 +376,7 @@ struct BatchDiffPass : public enzyme::impl::BatchDiffPassBase<BatchDiffPass> {
       auto fnOp = cast<FunctionOpInterface>(symbolOp);
 
       // skip if fn isn't readonly(iterate through toplevel ops)
-      if (!batchutils::isReadOnly(fnOp)) {
+      if (!oputils::isReadOnly(fnOp)) {
         return mlir::WalkResult::skip();
       }
 
