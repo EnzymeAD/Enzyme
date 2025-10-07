37fec1931b8755d10293c599b0b1fae9b4703452
Valentin Churavy <v.churavy@gmail.com>
2025-10-07 19:14:01 +0200

    fixup! add external_context to TypeAnalysis

--- enzyme/Enzyme/Enzyme.cpp
+++ enzyme/Enzyme/Enzyme.cpp
@@ -1710,7 +1710,7 @@ public:
     bool AtomicAdd = Arch == Triple::nvptx || Arch == Triple::nvptx64 ||
                      Arch == Triple::amdgcn;
 
-    TypeAnalysis TA(Logic.PPC.FAM);
+    TypeAnalysis TA(Logic.PPC.FAM, Logic.ExternalContext);
     FnTypeInfo type_args = populate_type_args(TA, fn, mode);
 
     IRBuilder Builder(CI);
@@ -2850,7 +2850,7 @@ public:
                     *CI->getArgOperand(0));
         return false;
       }
-      TypeAnalysis TA(Logic.PPC.FAM);
+      TypeAnalysis TA(Logic.PPC.FAM, Logic.ExternalContext);
 
       auto Arch =
           llvm::Triple(
