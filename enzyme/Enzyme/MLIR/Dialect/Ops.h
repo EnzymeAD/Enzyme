6395ac32568b9c4ce8495fc650e211b30bf78b4c
Vimarsh Sathia <vsathia2@illinois.edu>
2025-10-11 14:27:44 -0500

    Extra class declarations for primal and grad args

--- enzyme/Enzyme/MLIR/Dialect/Ops.h
+++ enzyme/Enzyme/MLIR/Dialect/Ops.h
@@ -9,6 +9,8 @@
 #ifndef ENZYMEOPS_H
 #define ENZYMEOPS_H
 
+#include <type_traits>
+
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
@@ -27,6 +29,68 @@
 
 #define GET_TYPEDEF_CLASSES
 #include "Dialect/EnzymeOpsTypes.h.inc"
+
+// forward declare Enzyme op definitions
+#include "Dialect/EnzymeOps.h.inc"
+
+namespace mlir {
+namespace enzyme {
+namespace detail {
+
+// For any differentiation op, we either return input primal values or
+// derivative values with activity `enzyme_dup` / `enzyme_dupnoneed`. In the
+// case of reverse mode differentiation, derivative values for outputs marked
+// `enzyme_active` / `enzyme_activenoneed` are also returned
+template <typename SourceOp, bool filterGrad>
+llvm::SmallVector<mlir::Value> filterGradInputs(SourceOp uop) {
+  llvm::SmallVector<mlir::Value, 2> outs;
+  auto in_idx = 0;
+
+  for (auto act : uop.getActivity()) {
+    auto iattr = cast<ActivityAttr>(act);
+    auto act_val = iattr.getValue();
+
+    if constexpr (!filterGrad) {
+      outs.push_back(uop.getInputs()[in_idx]);
+    }
+
+    ++in_idx;
+
+    if (act_val == Activity::enzyme_dup ||
+        act_val == Activity::enzyme_dupnoneed) {
+
+      if constexpr (filterGrad) {
+        outs.push_back(uop.getInputs()[in_idx]);
+      }
+
+      ++in_idx;
+    }
+  }
+
+  // For reverse mode AD, add derivative values corresponding to active outputs
+  if constexpr ((std::is_same_v<SourceOp, AutoDiffOp> ||
+                 std::is_same_v<SourceOp, AutoDiffRegionOp>) &&
+                filterGrad) {
+    if (in_idx != uop.getInputs().size()) {
+      for (auto act : uop.getRetActivity()) {
+        auto iattr = cast<ActivityAttr>(act);
+        auto act_val = iattr.getValue();
+
+        if (act_val == Activity::enzyme_active ||
+            act_val == Activity::enzyme_activenoneed) {
+          outs.push_back(uop.getInputs()[in_idx]);
+          in_idx++;
+        }
+      }
+    }
+  }
+
+  return outs;
+}
+
+} // namespace detail
+} // namespace enzyme
+} // namespace mlir
 
 #define GET_OP_CLASSES
 #include "Dialect/EnzymeOps.h.inc"
