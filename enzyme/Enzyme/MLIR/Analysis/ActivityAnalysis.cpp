2a0fc45de643aab4105df3a167557755dc57d178
Vimarsh Sathia <vsathia2@illinois.edu>
2025-10-12 23:02:34 -0500

    Undo changes to ActivityAnalysis

--- enzyme/Enzyme/MLIR/Analysis/ActivityAnalysis.cpp
+++ enzyme/Enzyme/MLIR/Analysis/ActivityAnalysis.cpp
@@ -256,12 +256,42 @@ static Operation *getFunctionFromCall(CallOpInterface iface) {
 
 constexpr bool EnzymePrintActivity = false;
 
+static bool isReadOnly(Operation *op) {
+  bool hasRecursiveEffects = op->hasTrait<OpTrait::HasRecursiveMemoryEffects>();
+  if (hasRecursiveEffects) {
+    for (Region &region : op->getRegions()) {
+      for (auto &block : region) {
+        for (auto &nestedOp : block)
+          if (!isReadOnly(&nestedOp))
+            return false;
+      }
+    }
+    return true;
+  }
+
+  // If the op has memory effects, try to characterize them to see if the op
+  // is trivially dead here.
+  if (auto effectInterface = dyn_cast<MemoryEffectOpInterface>(op)) {
+    // Check to see if this op either has no effects, or only allocates/reads
+    // memory.
+    SmallVector<MemoryEffects::EffectInstance, 1> effects;
+    effectInterface.getEffects(effects);
+    if (!llvm::all_of(effects, [op](const MemoryEffects::EffectInstance &it) {
+          return isa<MemoryEffects::Read>(it.getEffect());
+        })) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
 bool mlir::enzyme::ActivityAnalyzer::isReadOnly(Operation *val) {
   auto find = readOnlyCache.find(val);
   if (find != readOnlyCache.end()) {
     return find->second;
   }
-  auto res = oputils::isReadOnly(val);
+  auto res = ::isReadOnly(val);
   readOnlyCache[val] = res;
   return res;
 }
