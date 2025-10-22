8906b7aa401b8c23d8b9ed64060a27aeda227d0c
Paul Berg <9824244+Pangoraw@users.noreply.github.com>
2025-10-22 08:15:15 -0500

    fmt

--- enzyme/Enzyme/MLIR/Dialect/LLVMExt/LLVMExt.h
+++ enzyme/Enzyme/MLIR/Dialect/LLVMExt/LLVMExt.h
@@ -1,4 +1,4 @@
-//===- LLVMExt.h - LLVMExt dialect -------------------------------*- C++ -*-===//
+//===- LLVMExt.h - LLVMExt dialect ------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
