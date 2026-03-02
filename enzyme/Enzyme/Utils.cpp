816681ec74942417d1ac42f84b9ad0812d31dd77
William S. Moses <gh@wsmoses.com>
2026-03-02 16:15:36 -0600

    WIP: make backtrace

--- enzyme/Enzyme/Utils.cpp
+++ enzyme/Enzyme/Utils.cpp
@@ -525,6 +525,58 @@ Constant *getString(Module &M, StringRef Str) {
   return ConstantExpr::getInBoundsGetElementPtr(s->getType(), gv, Idxs);
 }
 
+void emit_backtrace(llvm::Instruction *inst, llvm::raw_ostream &ss) {
+    SmallPtrSet<llvm::Instruction*> done;
+    while (true) {
+        if (done.contains(inst)) break;
+        done.insert(inst);
+
+
+        if haskey(metadata(inst), LLVM.MD_dbg)
+            loc = metadata(inst)[LLVM.MD_dbg]
+            while loc !== nothing
+                scope = LLVM.scope(loc)
+                if scope !== nothing
+                    name = replace(LLVM.name(scope), r";$"=>"")
+                    file = LLVM.file(scope)
+                    path = joinpath(LLVM.directory(file), LLVM.filename(file))
+                    line = LLVM.line(loc)
+                    push!(bt, StackTraces.StackFrame(name, path, line))
+                end
+                loc = LLVM.inlined_at(loc)
+            end
+        end
+
+        # move up the call chain
+        f = LLVM.parent(LLVM.parent(inst))
+        ## functions can be used as a *value* in eg. constant expressions, so filter those out
+        callers = filter(val -> isa(user(val), LLVM.CallInst), collect(uses(f)))
+        ## get rid of calls without debug info
+        filter!(callers) do call
+            md = metadata(user(call))
+            haskey(md, LLVM.MD_dbg)
+        end
+        if !isempty(callers)
+            # figure out the call sites of this instruction
+            call_sites = unique(callers) do call
+                # there could be multiple calls, originating from the same source location
+                md = metadata(user(call))
+                md[LLVM.MD_dbg]
+            end
+
+            if length(call_sites) > 1
+                frame = StackTraces.StackFrame("multiple call sites", "unknown", 0)
+                push!(bt, frame)
+            elseif length(call_sites) == 1
+                inst = user(first(call_sites))
+                continue
+            end
+        end
+        break
+    end
+    }
+}
+
 void ErrorIfRuntimeInactive(llvm::IRBuilder<> &B, llvm::Value *primal,
                             llvm::Value *shadow, const char *Message,
                             llvm::DebugLoc &&loc, llvm::Instruction *orig) {
