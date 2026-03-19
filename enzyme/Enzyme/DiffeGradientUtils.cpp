7bc42fa7c1219855789f15ba17003a565d2dc6d4
Billy Moses <gh@wsmoses.com>
2026-03-19 19:09:47 +0000

    Fix dead store alias issue

--- enzyme/Enzyme/DiffeGradientUtils.cpp
+++ enzyme/Enzyme/DiffeGradientUtils.cpp
@@ -1032,12 +1032,6 @@ void DiffeGradientUtils::addToInvertedPtrDiffe(Instruction *orig,
 
       SmallVector<Metadata *, 1> scopeMD = {
           getDerivativeAliasScope(origptr, idx)};
-      if (auto origValI = dyn_cast_or_null<Instruction>(origVal))
-        if (auto MD = origValI->getMetadata(LLVMContext::MD_alias_scope)) {
-          auto MDN = cast<MDNode>(MD);
-          for (auto &o : MDN->operands())
-            scopeMD.push_back(o);
-        }
       auto scope = MDNode::get(LI->getContext(), scopeMD);
       LI->setMetadata(LLVMContext::MD_alias_scope, scope);
       st->setMetadata(LLVMContext::MD_alias_scope, scope);
@@ -1047,12 +1041,6 @@ void DiffeGradientUtils::addToInvertedPtrDiffe(Instruction *orig,
         if (j != (ssize_t)idx)
           MDs.push_back(getDerivativeAliasScope(origptr, j));
       }
-      if (auto origValI = dyn_cast_or_null<Instruction>(origVal))
-        if (auto MD = origValI->getMetadata(LLVMContext::MD_noalias)) {
-          auto MDN = cast<MDNode>(MD);
-          for (auto &o : MDN->operands())
-            MDs.push_back(o);
-        }
       idx++;
       auto noscope = MDNode::get(ptr->getContext(), MDs);
       LI->setMetadata(LLVMContext::MD_noalias, noscope);
