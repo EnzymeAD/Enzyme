739c3c73f37a5c397ed83c33a2c1c8101afdc19c
Billy Moses <wmoses@google.com>
2026-05-10 11:57:00 -0500

    CAPI: Add gradient utils logic getter

--- enzyme/Enzyme/CApi.cpp
+++ enzyme/Enzyme/CApi.cpp
@@ -436,6 +436,10 @@ uint64_t EnzymeGradientUtilsGetWidth(GradientUtils *gutils) {
   return gutils->getWidth();
 }
 
+EnzymeLogicRef EnzymeGradientUtilsGetLogic(GradientUtils *gutils) {
+  return (EnzymeLogicRef)&gutils->Logic;
+}
+
 LLVMTypeRef EnzymeGradientUtilsGetShadowType(GradientUtils *gutils,
                                              LLVMTypeRef T) {
   return wrap(gutils->getShadowType(unwrap(T)));
