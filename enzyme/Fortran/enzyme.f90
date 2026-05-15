9bd7ad1e3c44fd532aa623bfaed5917497744d00
Joe Wallwork <22053413+joewallwork@users.noreply.github.com>
2026-05-15 17:06:56 +0100

    Create Fortran bindings module (#2807)
    * Add enzyme_mod containing activity descriptor bindings
    
    * Hook Fortran bindings up in build system
    
    * Rename enzyme_mod->enzyme
    
    * Use enable_language in Fortran subdir
    
    * Add ENZYME_FORTRAN option for building bindings

---
+++ enzyme/Fortran/enzyme.f90
@@ -0,0 +1,36 @@
+! ===- enzyme.f90 - Fortran bindings for Enzyme ---------------------------=== !
+!
+!                              Enzyme Project
+!
+!  Part of the Enzyme Project, under the Apache License v2.0 with LLVM
+!  Exceptions. See https://llvm.org/LICENSE.txt for license information.
+!  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+!
+!  If using this code in an academic setting, please cite the following:
+!  @misc{enzymeGithub,
+!   author = {William S. Moses and Valentin Churavy},
+!   title = {Enzyme: High Performance Automatic Differentiation of LLVM},
+!   year = {2020},
+!   howpublished = {\url{https://github.com/wsmoses/Enzyme}},
+!   note = {commit xxxxxxx}
+!  }
+!
+! ===----------------------------------------------------------------------=== !
+!
+!  This file provides Fortran bindings for Enzyme.
+!
+! ===----------------------------------------------------------------------=== !
+module enzyme
+  use iso_c_binding, only: c_int
+  implicit none
+  private
+
+  ! Bindings for activity descriptors
+  integer(c_int), public, bind(C, name="enzyme_const")     :: enzyme_const
+  integer(c_int), public, bind(C, name="enzyme_dup")       :: enzyme_dup
+  integer(c_int), public, bind(C, name="enzyme_dupnoneed") :: enzyme_dupnoneed
+  integer(c_int), public, bind(C, name="enzyme_out")       :: enzyme_out
+  integer(c_int), public, bind(C, name="enzyme_scalar")    :: enzyme_scalar
+  integer(c_int), public, bind(C, name="enzyme_width")     :: enzyme_width
+  integer(c_int), public, bind(C, name="enzyme_vector")    :: enzyme_vector
+end module enzyme
