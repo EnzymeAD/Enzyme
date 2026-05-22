ba0c1fa1e5829bb79f0f58896e8bd8053716daa9
xys-syx <xuyuansui@outlook.com>
2026-05-21 21:04:45 -0500

    load linalg dialect (#2838)

--- enzyme/Enzyme/MLIR/Passes/EnzymeMLIRPass.cpp
+++ enzyme/Enzyme/MLIR/Passes/EnzymeMLIRPass.cpp
@@ -17,6 +17,7 @@
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/Interfaces/FunctionInterfaces.h"
 #include "mlir/Pass/PassManager.h"
@@ -52,7 +53,7 @@ struct DifferentiatePass
 
     registry.insert<mlir::arith::ArithDialect, mlir::complex::ComplexDialect,
                     mlir::cf::ControlFlowDialect, mlir::tensor::TensorDialect,
-                    mlir::enzyme::EnzymeDialect>();
+                    mlir::linalg::LinalgDialect, mlir::enzyme::EnzymeDialect>();
   }
 
   static std::vector<DIFFE_TYPE> mode_from_fn(FunctionOpInterface fn,
