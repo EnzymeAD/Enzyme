e867c68ebf62fab14af68a38953c52f89d65d5fa
Joe Wallwork <jw2423@cam.ac.uk>
2026-06-08 15:15:32 +0100

    Add notes in tests

--- enzyme/test/Fortran/ReverseMode/square.f90
+++ enzyme/test/Fortran/ReverseMode/square.f90
@@ -3,6 +3,10 @@
 ! RUN: if [[ %llvmver -ge 13 && %fc != ifx ]]; then %fc -flto -O2 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o /dev/stdout | %opt -O2 -S -o %t.ll && %fc -flto -O2 %t.ll -o %t1 && %t1 | FileCheck %s; fi
 ! RUN: if [[ %llvmver -ge 13 && %fc != ifx ]]; then %fc -flto -O3 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o /dev/stdout | %opt -O3 -S -o %t.ll && %fc -flto -O3 %t.ll -o %t1 && %t1 | FileCheck %s; fi
 
+! NOTE: This test is only configured to run with the flang compiler
+!       For it to work with the ifx compiler we will need to figure out how to
+!       handle the indirection involved in the enzyme_autodiff binding
+
 module math
 contains
     real function square( x )
