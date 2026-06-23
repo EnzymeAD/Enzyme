a40c4ceaf22f0959061a27bc9313f6dd7a672f04
Julian Andrej <andrej1@llnl.gov>
2026-06-23 10:24:58 -0700

    mfem integration testing

--- integration/mfem/run.sh
+++ integration/mfem/run.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
-echo "Running MFEM test"
+echo "Running MFEM integration tests"
 
-# $1 is the first argument, $2 is the second argument
 CLANG=$1
 CLANGENZYME=$2
 
 echo "$CLANG" "$CLANGENZYME"
+
+apt install -y openmpi-bin openmpi-common libopenmpi-dev libhypre-dev
+
+git clone -b dfem-dev --single-branch https://github.com/mfem/mfem.git
+cd mfem
+git apply --check ../Enzyme/integration/mfem/mfem.patch
+
+# if [ -d "build" ]; then
+#     rm -rf build
+# else
+
+echo $PWD
+mkdir build
+cd build
+CXX=clang++-$CLANG cmake .. \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_CXX_STANDARD=17 \
+-DCMAKE_CXX_STANDARD_REQUIRED=ON \
+-DMFEM_USE_MPI=ON \
+-DHYPRE_INCLUDE_DIR=/usr/include/hypre \
+-DMETIS_INCLUDE_DIR=/usr/include \
+-DMFEM_USE_ENZYME=ON \
+-DENZYME_DIR=$CLANGENZYME
+
+# make -j `nprocs`
+echo $PWD
+make -j 4
+
+echo $PWD
+cd tests
+make -j 4 -C .. punit_tests && ./unit/punit_tests "[dFEM]"
