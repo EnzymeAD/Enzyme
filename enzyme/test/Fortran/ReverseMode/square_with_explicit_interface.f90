45f814724571bc5f41e425d8b5291cc82cef0a50
Joe Wallwork <jw2423@cam.ac.uk>
2026-06-25 15:20:24 +0100

    Add missing fortran requirement and drop loadFortran when not needed

--- enzyme/test/Fortran/ReverseMode/square_with_explicit_interface.f90
+++ enzyme/test/Fortran/ReverseMode/square_with_explicit_interface.f90
@@ -1,3 +1,4 @@
+! REQUIRES: fortran
 ! RUN: %fc -flto -O0 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o /dev/stdout | %opt %oflags -O0 -S -o %t.ll && %fc -flto -O0 %t.ll -o %t1 && %t1 | FileCheck %s
 ! RUN: %fc -flto -O1 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o /dev/stdout | %opt %oflags -O1 -S -o %t.ll && %fc -flto -O1 %t.ll -o %t1 && %t1 | FileCheck %s
 ! RUN: %fc -flto -O2 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o /dev/stdout | %opt %oflags -O2 -S -o %t.ll && %fc -flto -O2 %t.ll -o %t1 && %t1 | FileCheck %s
