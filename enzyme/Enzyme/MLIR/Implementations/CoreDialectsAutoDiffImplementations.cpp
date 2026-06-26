cffeb4ad7771610fed2b9397d1de345d1dec6775
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-06-26 07:51:40 -0500

    Revert "add LLVM version conditional code"
    This reverts commit d4c4f391839bd22fafb6fb7050eacd7d5fc5b464.

--- enzyme/Enzyme/MLIR/Implementations/CoreDialectsAutoDiffImplementations.cpp
+++ enzyme/Enzyme/MLIR/Implementations/CoreDialectsAutoDiffImplementations.cpp
@@ -299,11 +299,7 @@ void mlir::enzyme::detail::regionTerminatorForwardHandler(
     for (auto &successor : successors) {
       OperandRange operandRange = termIface.getSuccessorOperands(successor);
       ValueRange targetValues =
-        #if LLVM_VERSION_MAJOR > 23
           successor.isOperation()
-        #else
-          successor.isParent()
-        #endif
               ? parentOp->getResults()
               : regionBranchOp.getSuccessorInputs(successor);
       assert(operandRange.size() == targetValues.size());
@@ -369,13 +365,8 @@ LogicalResult mlir::enzyme::detail::controlFlowForwardHandler(
         iface.getSuccessorOperands(regionBranchOp, successor);
 
     ValueRange targetValues =
-      #if LLVM_VERSION_MAJOR > 23
-        successor.isOperation()
-      #else
-        successor.isParent()
-      #endif
-          ? op->getResults()
-          : regionBranchOp.getSuccessorInputs(successor);
+        successor.isOperation() ? op->getResults()
+                                : regionBranchOp.getSuccessorInputs(successor);
 
     // Need to know which of the arguments are being forwarded to from
     // operands.
@@ -384,11 +375,7 @@ LogicalResult mlir::enzyme::detail::controlFlowForwardHandler(
       if (gutils->isConstantValue(regionValue))
         continue;
       operandPositionsToShadow.insert(operandRange.getBeginOperandIndex() + i);
-      #if LLVM_VERSION_MAJOR > 23
       if (successor.isOperation())
-      #else
-      if (successor.isParent())
-      #endif
         resultPositionsToShadow.insert(i);
     }
   }
