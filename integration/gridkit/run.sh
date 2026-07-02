f5c6bc5c69edf4812d144f5c3fefa2488572a2e6
Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
2026-07-02 17:21:56 -0400

    GridKit integration test (#2911)
    * GridKit integration test.
    
    * Add PWD to Enzyme path argument.
    
    * Fix GridKit commit.
    
    * Exclude gridkit+gpu.
    
    * Use https to clone GridKit.
    
    * Fix PWD.
    
    * Specify GridKit build type.
    
    * ls ENZYME_DIR and try searching in a directory abobve.
    
    * Better check for directory above.
    
    * Fix ENZYME_DIR passed to integration tests.
    
    * Back to using ENZYME_DIR directly for GridKit test.
    
    * Update GridKit submodules before building.

---
+++ integration/gridkit/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+echo "Running GridKit integration tests"
+
+CLANGV=$1
+ENZYME_DIR=$2
+
+echo "CLANGV: $CLANGV" 
+echo "ENZYME_DIR: $ENZYME_DIR"
+
+git clone https://github.com/ORNL/GridKit.git
+mkdir GridKit/build
+cd GridKit/build
+git checkout ea96fba7bcc9c9cc402eafd7ec3311bb08889598
+git submodule update --init
+
+cmake \
+  -DCMAKE_CXX_COMPILER=clang++-$CLANGV \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DGRIDKIT_ENABLE_ENZYME=On \
+  -DENZYME_DIR=${ENZYME_DIR} \
+  ..
+
+make -j
+make test
