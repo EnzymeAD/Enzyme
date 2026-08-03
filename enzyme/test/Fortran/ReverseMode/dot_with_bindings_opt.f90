ccc8589194cb5b525d996ec131c656eba0129df5
Joe Wallwork <jw2423@cam.ac.uk>
2026-08-03 14:40:55 +0100

    Separate out dot test

---
+++ enzyme/test/Fortran/ReverseMode/dot_with_bindings_opt.f90
@@ -0,0 +1,81 @@
+! REQUIRES: fortran
+! RUN: %fc -flto -O1 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O1 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %fc -flto -O2 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O2 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %fc -flto -O3 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O3 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %if flangenzyme %{ %fc -O2 %loadFortran %loadFlangEnzyme %s -o %t2 && %t2 | FileCheck %s %}
+
+program main
+  use enzyme, only: enzyme_const, enzyme_dup, enzyme_autodiff
+  implicit none
+
+  integer, parameter :: n = 20000000
+  integer, parameter :: s = 20
+  real :: x(n), y(n), z = 1 / s
+  real :: dx(n), dy(n), dz
+  integer :: i
+
+  ! Specify input
+  do i = 1, n
+    x(i) = s / i
+    y(i) = s + i - 1 ! NOTE: Minus one to account for 1-indexing
+  end do
+
+  ! Compute gradient computation with respect to all variables (x, y, and z)
+  dx(:) = 0.0
+  dy(:) = 0.0
+  dz = 0.0
+  call enzyme_autodiff(dot, enzyme_const, n, &
+                       enzyme_dup, x, dx, &
+                       enzyme_dup, y, dy, &
+                       enzyme_dup, z, dz)
+  write(*, "(f4.1)") dx(1)
+  write(*, "(f4.1)") dy(1)
+  write(*, "(f4.1)") dz
+
+  ! Compute gradient computation with respect to just y and z
+  dx(:) = 0.0
+  dy(:) = 0.0
+  dz = 0.0
+  call enzyme_autodiff(dot, enzyme_const, n, &
+                       enzyme_const, x, &
+                       enzyme_dup, y, dy, &
+                       enzyme_dup, z, dz)
+  write(*, "(f4.1)") dx(1)
+  write(*, "(f4.1)") dy(1)
+  write(*, "(f4.1)") dz
+
+  ! Compute gradient computation with respect to just z
+  dx(:) = 0.0
+  dy(:) = 0.0
+  dz = 0.0
+  call enzyme_autodiff(dot, enzyme_const, n, &
+                       enzyme_const, x, &
+                       enzyme_const, y, &
+                       enzyme_dup, z, dz)
+  write(*, "(f4.1)") dx(1)
+  write(*, "(f4.1)") dy(1)
+  write(*, "(f4.1)") dz
+
+contains
+
+  ! Function for computing the dot product of two vectors and adding a scalar
+  real function dot(n, a, b, c)
+    integer, intent(in) :: n
+    real, dimension(n), intent(in) :: a
+    real, dimension(n), intent(in) :: b
+    real, intent(in) :: c
+    integer :: i
+    dot = dot_product(a, b) + c
+  end function
+
+end program
+
+! CHECK: 20.0
+! CHECK-NEXT: 20.0
+! CHECK-NEXT: 1.0
+! CHECK-NEXT: 0.0
+! CHECK-NEXT: 20.0
+! CHECK-NEXT: 1.0
+! CHECK-NEXT: 0.0
+! CHECK-NEXT: 0.0
+! CHECK-NEXT: 1.0
