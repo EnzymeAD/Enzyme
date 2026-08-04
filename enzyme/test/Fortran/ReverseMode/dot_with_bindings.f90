10dab7fc92d1213a98f959e3727b2b08ee04b047
Joe Wallwork <jw2423@cam.ac.uk>
2026-08-04 13:27:03 +0100

    Add flang plugin test variants for dot and norm examples

--- enzyme/test/Fortran/ReverseMode/dot_with_bindings.f90
+++ enzyme/test/Fortran/ReverseMode/dot_with_bindings.f90
@@ -3,6 +3,8 @@
 ! RUN: %fc -flto -O1 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O1 %t.ll -o %t1 && %t1 | FileCheck %s
 ! RUN: %fc -flto -O2 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O2 %t.ll -o %t1 && %t1 | FileCheck %s
 ! RUN: %fc -flto -O3 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O3 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %if flangenzyme %{ %fc -O0 %loadFortran %loadFlangEnzyme %s -o %t2 && %t2 | FileCheck %s %}
+! RUN: %if flangenzyme %{ %fc -O2 %loadFortran %loadFlangEnzyme %s -o %t2 && %t2 | FileCheck %s %}
 
 ! NOTE: This test is only configured to run with the flang compiler at -O0
 !       For it to work with the ifx compiler we will need to figure out how to
