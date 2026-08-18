550fd954897de88743597146c3ff01e2eeda866d
isaac <ia464@cam.ac.uk>
2026-08-18 11:06:20 +0100

    Fix formatting

--- enzyme/Enzyme/PreserveNVVM.cpp
+++ enzyme/Enzyme/PreserveNVVM.cpp
@@ -407,8 +407,7 @@ bool preserveNVVM(bool Begin, Module &M,
 
           auto *Hook =
               dyn_cast<Function>(Call->getCalledOperand()->stripPointerCasts());
-          if (!Hook ||
-              !startsWith(Hook->getName(), "f__enzyme_function_like"))
+          if (!Hook || !startsWith(Hook->getName(), "f__enzyme_function_like"))
             continue;
 
           if (Call->arg_size() != 2) {
@@ -748,9 +747,8 @@ bool preserveNVVM(bool Begin, Module &M,
           if (MarkerPos == StringRef::npos || !isa<Function>(Target))
             continue;
 
-          handleFunctionLike(
-              Begin, Target,
-              g.getName().substr(MarkerPos + Marker.size()));
+          handleFunctionLike(Begin, Target,
+                             g.getName().substr(MarkerPos + Marker.size()));
           toErase.push_back(&g);
           changed = true;
           continue;
