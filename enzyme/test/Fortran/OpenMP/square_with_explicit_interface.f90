51d4cc42340b48752431e5607433473d340c1f53
Joe Wallwork <jw2423@cam.ac.uk>
2026-08-21 10:43:31 +0100

    Add version with explicit interface

---
+++ enzyme/test/Fortran/OpenMP/square_with_explicit_interface.f90
@@ -0,0 +1,71 @@
+! REQUIRES: fortran
+! RUN: %fc -flto -O0 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O0 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %fc -flto -O1 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O1 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %fc -flto -O2 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O2 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %fc -flto -O3 -c %loadFortran %s -o /dev/stdout | %opt %loadEnzyme %enzyme -o %t.ll && %fc -flto -O3 %t.ll -o %t1 && %t1 | FileCheck %s
+! RUN: %if flangenzyme %{ %fc -O0 %loadFortran %loadFlangEnzyme %s -o %t2 && %t2 | FileCheck %s %}
+! RUN: %if flangenzyme %{ %fc -O2 %loadFortran %loadFlangEnzyme %s -o %t2 && %t2 | FileCheck %s %}
+
+module squareOMP
+  implicit none
+  public
+  interface
+    subroutine square__enzyme_autodiff(sr, n, x, dx, y, dy)
+      implicit none
+      interface
+        subroutine sr_decal(n, a, b)
+          implicit none
+          integer, value, intent(in) :: n
+          real, intent(in) :: a(n)
+          real, intent(out) :: b(n)
+        end subroutine sr_decal
+      end interface
+      procedure(sr_decal) :: sr
+      integer, value, intent(in) :: n
+      real, intent(in) :: x(n)
+      real, intent(inout) :: dx(n)
+      real, intent(out) :: y(n)
+      real, intent(inout) :: dy(n)
+    end subroutine square__enzyme_autodiff
+  end interface
+contains
+  subroutine square(n, x, y)
+    integer, value, intent(in) :: n
+    real, intent(in) :: x(n)
+    real, intent(out) :: y(n)
+    integer :: i
+    !$omp parallel do
+    do i = 1, n
+      y(i) = x(i)**2
+    end do
+  end subroutine square
+end module squareOMP
+
+program main
+  use squareOMP, only: square, square__enzyme_autodiff
+  implicit none
+
+  integer, parameter :: n = 4
+  real :: x(n), dx(n)
+  real :: y(n), dy(n)
+  integer :: i
+
+  x(1) = 23.1
+  x(2) = 10.0
+  x(3) = 100.0
+  x(4) = 3.14
+
+  dx(:) = 0.0
+  dy(:) = 1.0
+  call square__enzyme_autodiff(square, n, x, dx, y, dy)
+
+  write(*,"(f0.2)") dx(1)
+  write(*,"(f0.2)") dx(2)
+  write(*,"(f0.2)") dx(3)
+  write(*,"(f0.2)") dx(4)
+end program main
+
+! CHECK: 46.20
+! CHECK-NEXT: 20.00
+! CHECK-NEXT: 200.00
+! CHECK-NEXT: 6.28
