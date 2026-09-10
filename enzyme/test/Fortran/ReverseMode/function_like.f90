0727c8972bd31c215cb7a66ba6e70971a15b165a
isaac <ia464@cam.ac.uk>
2026-09-10 14:38:55 +0100

    Remove the use of modules, improve naming

--- enzyme/test/Fortran/ReverseMode/function_like.f90
+++ enzyme/test/Fortran/ReverseMode/function_like.f90
@@ -4,40 +4,35 @@
 ! RUN: %if flangenzyme %{ %fc -O0 %loadFortran %loadFlangEnzyme %s -o %t2 && %t2 | FileCheck %s %}
 ! RUN: %if flangenzyme %{ %fc -O2 %loadFortran %loadFlangEnzyme %s -o %t2 && %t2 | FileCheck %s %}
 
-module function_like_test
-  implicit none
-
-contains
-
-  function log1p_like_function(x) result(y)
-    real, value :: x
-    real :: y
-
-    y = 2.0 * x
-  end function log1p_like_function
-
-  function test(x) result(y)
-    real, intent(in) :: x
-    real :: y
-
-    y = log1p_like_function(x)
-  end function test
-
-end module function_like_test
-
 program main
   use enzyme, only: enzyme_autodiff, enzyme_function_like, enzyme_log1p
-  use function_like_test, only: log1p_like_function, test
   implicit none
 
   real :: x, dx
 
   x = 2.0
   dx = 0.0
-  call enzyme_function_like(log1p_like_function, enzyme_log1p)
+  call enzyme_function_like(double_value, enzyme_log1p)
   call enzyme_autodiff(test, x, dx)
 
   write(*,"(f6.4)") dx
+
+contains
+
+  function double_value(x) result(y)
+    real, value :: x
+    real :: y
+
+    y = 2.0 * x
+  end function double_value
+
+  function test(x) result(y)
+    real, intent(in) :: x
+    real :: y
+
+    y = double_value(x)
+  end function test
+
 end program main
 
 ! CHECK: 0.3333
